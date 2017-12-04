17e49ddd0a0d146347e53df413fd8ed028bcea2a
Tim 'mithro' Ansell <mithro@mithis.com>
2017-12-04 20:43:48 +0000

    Travis build #471 of fixing-unstable-link/v0.0.0-1881-gdf01395 for PLATFORM=opsis TARGET=base CPU=lm32
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/df01395510a40cb00c89bcb2b57cc60e2076506e

---
+++ archive/fixing-unstable-link/v0.0.0-1881-gdf01395/opsis/base/lm32/logs/version_data.c
@@ -0,0 +1,21 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "df01395510a40cb00c89bcb2b57cc60e2076506e";
+const char* git_branch = "fixing-unstable-link";
+const char* git_describe = "v0.0.0-1881-gdf01395-dirty";
+const char* git_status =
+    "    --\r\n"
+   "     ? ../../../../third_party/litepcie\r\n"
+   "     ? ../../../../third_party/litesata\r\n"
+   "     ? ../../../../third_party/liteusb\r\n"
+    "    --\r\n";
+
