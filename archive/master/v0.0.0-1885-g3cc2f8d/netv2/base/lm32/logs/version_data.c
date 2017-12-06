13c98f9e0997220fc24589edad0daf02bd05899b
Tim 'mithro' Ansell <mithro@mithis.com>
2017-12-06 08:28:06 +0000

    Travis build #474 of master/v0.0.0-1885-g3cc2f8d for PLATFORM=netv2 TARGET=base CPU=lm32
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/3cc2f8dd0272f068fd609a8a8efc79455b5701ef

---
+++ archive/master/v0.0.0-1885-g3cc2f8d/netv2/base/lm32/logs/version_data.c
@@ -0,0 +1,21 @@
+
+#ifndef PLATFORM_NETV2
+#error "Version mismatch - PLATFORM_NETV2 not defined!"
+#endif
+const char* board = "netv2";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "3cc2f8dd0272f068fd609a8a8efc79455b5701ef";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.0-1885-g3cc2f8d-dirty";
+const char* git_status =
+    "    --\r\n"
+   "     ? ../../../../third_party/litepcie\r\n"
+   "     ? ../../../../third_party/litesata\r\n"
+   "     ? ../../../../third_party/liteusb\r\n"
+    "    --\r\n";
+
