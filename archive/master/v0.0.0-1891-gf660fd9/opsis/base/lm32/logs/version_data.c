5f6f3800c7bd0b9f0e49d5e24d47e2ada56506ab
Tim 'mithro' Ansell <me@mith.ro>
2017-12-29 22:40:29 +0000

    Travis build #496 of master/v0.0.0-1891-gf660fd9 for PLATFORM=opsis TARGET=base CPU=lm32
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/f660fd945dbdc061f1fabe93dd9cb03494058c5f

---
+++ archive/master/v0.0.0-1891-gf660fd9/opsis/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "f660fd945dbdc061f1fabe93dd9cb03494058c5f";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.0-1891-gf660fd9";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
