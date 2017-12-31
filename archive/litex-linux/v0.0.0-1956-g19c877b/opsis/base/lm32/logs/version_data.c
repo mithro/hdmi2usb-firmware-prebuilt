45de994a1f2d2e73d8d48bed9aaf4d9084305eeb
Tim 'mithro' Ansell <me@mith.ro>
2017-12-31 12:22:33 +0000

    Travis build #501 of litex-linux/v0.0.0-1956-g19c877b for PLATFORM=opsis TARGET=base CPU=lm32 FIRMWARE=
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/19c877b40b5b4beca82976ce721944233a6f4555

---
+++ archive/litex-linux/v0.0.0-1956-g19c877b/opsis/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "19c877b40b5b4beca82976ce721944233a6f4555";
+const char* git_branch = "litex-linux";
+const char* git_describe = "v0.0.0-1956-g19c877b";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
