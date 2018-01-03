ff44e4972d24c24ed3ccd83aa7b11d56d468f3b4
Tim 'mithro' Ansell <me@mith.ro>
2018-01-03 15:43:59 +0000

    Travis build #525 of litex-linux/v0.0.0-1956-ge17337c for PLATFORM=arty TARGET=net CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/e17337c9c050d462796bd395cde2fe7523671570

---
+++ archive/litex-linux/v0.0.0-1956-ge17337c/arty/net/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "e17337c9c050d462796bd395cde2fe7523671570";
+const char* git_branch = "litex-linux";
+const char* git_describe = "v0.0.0-1956-ge17337c";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
