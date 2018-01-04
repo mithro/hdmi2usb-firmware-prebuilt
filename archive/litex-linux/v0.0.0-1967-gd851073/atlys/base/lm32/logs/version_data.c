7be91053b443aadf120b858016c26374d2789bde
Tim 'mithro' Ansell <me@mith.ro>
2018-01-04 03:20:04 +0000

    Travis build #528 of litex-linux/v0.0.0-1967-gd851073 for PLATFORM=atlys TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/d851073e4214aaa13958fa5d8c537a8b7450050a

---
+++ archive/litex-linux/v0.0.0-1967-gd851073/atlys/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "d851073e4214aaa13958fa5d8c537a8b7450050a";
+const char* git_branch = "litex-linux";
+const char* git_describe = "v0.0.0-1967-gd851073";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
