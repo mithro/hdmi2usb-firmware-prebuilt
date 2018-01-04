374552b1cbb8b4610c8538d20e1926c8788e2ea5
Tim 'mithro' Ansell <me@mith.ro>
2018-01-04 02:24:15 +0000

    Travis build #528 of litex-linux/v0.0.0-1967-gd851073 for PLATFORM=nexys_video TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/d851073e4214aaa13958fa5d8c537a8b7450050a

---
+++ archive/litex-linux/v0.0.0-1967-gd851073/nexys_video/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NEXYS_VIDEO
+#error "Version mismatch - PLATFORM_NEXYS_VIDEO not defined!"
+#endif
+const char* board = "nexys_video";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "d851073e4214aaa13958fa5d8c537a8b7450050a";
+const char* git_branch = "litex-linux";
+const char* git_describe = "v0.0.0-1967-gd851073";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
