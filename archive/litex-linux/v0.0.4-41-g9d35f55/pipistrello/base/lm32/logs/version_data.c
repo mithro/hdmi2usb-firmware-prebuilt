8cd2f422a4bd1220df91073983ef860ca71d67a8
Tim 'mithro' Ansell <me@mith.ro>
2018-01-10 03:25:14 +0000

    Travis build #535 of litex-linux/v0.0.4-41-g9d35f55 for PLATFORM=pipistrello TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/9d35f558679e7f07b0cdb67998e5e814115f6cc2

---
+++ archive/litex-linux/v0.0.4-41-g9d35f55/pipistrello/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_PIPISTRELLO
+#error "Version mismatch - PLATFORM_PIPISTRELLO not defined!"
+#endif
+const char* board = "pipistrello";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "9d35f558679e7f07b0cdb67998e5e814115f6cc2";
+const char* git_branch = "litex-linux";
+const char* git_describe = "v0.0.4-41-g9d35f55";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
