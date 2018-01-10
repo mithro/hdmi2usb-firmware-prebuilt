afb1bc212c66e5e3f47e4ffd4b47dc8670809747
Tim 'mithro' Ansell <me@mith.ro>
2018-01-10 04:05:50 +0000

    Travis build #536 of docker-attempt2/v0.0.4-42-g7a604b3 for PLATFORM=atlys TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/7a604b31884a702b6a4d3e2f0815c428c871e1ae

---
+++ archive/docker-attempt2/v0.0.4-42-g7a604b3/atlys/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "7a604b31884a702b6a4d3e2f0815c428c871e1ae";
+const char* git_branch = "docker-attempt2";
+const char* git_describe = "v0.0.4-42-g7a604b3";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
