3dd9513bdbae968d2ca7d24f02dad944a53fb382
Tim 'mithro' Ansell <me@mith.ro>
2018-01-10 03:06:43 +0000

    Travis build #535 of litex-linux/v0.0.0-1968-g9d35f55 for PLATFORM=minispartan6 TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/9d35f558679e7f07b0cdb67998e5e814115f6cc2

---
+++ archive/litex-linux/v0.0.0-1968-g9d35f55/minispartan6/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MINISPARTAN6
+#error "Version mismatch - PLATFORM_MINISPARTAN6 not defined!"
+#endif
+const char* board = "minispartan6";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "9d35f558679e7f07b0cdb67998e5e814115f6cc2";
+const char* git_branch = "litex-linux";
+const char* git_describe = "v0.0.0-1968-g9d35f55";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
