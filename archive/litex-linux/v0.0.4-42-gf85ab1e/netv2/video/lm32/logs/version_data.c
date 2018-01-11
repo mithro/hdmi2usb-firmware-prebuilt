c2f08a00f4cdb22e64502c36325bfd1d63971408
Tim 'mithro' Ansell <me@mith.ro>
2018-01-11 08:27:21 +0000

    Travis build #539 of litex-linux/v0.0.4-42-gf85ab1e for PLATFORM=netv2 TARGET=video CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/f85ab1e37c1e3daa2ab3e532c24801db6cdfd6b7

---
+++ archive/litex-linux/v0.0.4-42-gf85ab1e/netv2/video/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NETV2
+#error "Version mismatch - PLATFORM_NETV2 not defined!"
+#endif
+const char* board = "netv2";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "f85ab1e37c1e3daa2ab3e532c24801db6cdfd6b7";
+const char* git_branch = "litex-linux";
+const char* git_describe = "v0.0.4-42-gf85ab1e";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
