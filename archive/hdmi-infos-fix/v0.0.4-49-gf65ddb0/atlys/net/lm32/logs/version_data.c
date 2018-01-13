1fad717437a0e3c06f81aafc14741b4d0d96f15f
Tim 'mithro' Ansell <me@mith.ro>
2018-01-13 12:56:06 +0000

    Travis build #548 of hdmi-infos-fix/v0.0.4-49-gf65ddb0 for PLATFORM=atlys TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/f65ddb00f90d5f0a0251ee5e75d022d5be53b081

---
+++ archive/hdmi-infos-fix/v0.0.4-49-gf65ddb0/atlys/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "f65ddb00f90d5f0a0251ee5e75d022d5be53b081";
+const char* git_branch = "hdmi-infos-fix";
+const char* git_describe = "v0.0.4-49-gf65ddb0";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
