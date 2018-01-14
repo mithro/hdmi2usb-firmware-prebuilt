43b8be43831f41be7e017fc9bcc4d7e868060aff
Tim 'mithro' Ansell <me@mith.ro>
2018-01-14 10:47:59 +0000

    Travis build #553 of master/v0.0.4-60-gc443e9b for PLATFORM=mimasv2 TARGET=base CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/c443e9b27d8d325cde07630150d0190d217b0c84

---
+++ archive/master/v0.0.4-60-gc443e9b/mimasv2/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MIMASV2
+#error "Version mismatch - PLATFORM_MIMASV2 not defined!"
+#endif
+const char* board = "mimasv2";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "c443e9b27d8d325cde07630150d0190d217b0c84";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-60-gc443e9b";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
