8129bd3ee609d7ad1f30decbfabd8643dc2f7c7c
Tim 'mithro' Ansell <me@mith.ro>
2018-01-15 00:25:02 +0000

    Travis build #557 of wbtc-fix/v0.0.4-64-g58dd950 for PLATFORM=arty TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/58dd95053999cfe88b43c565d544e4011ed42fbf

---
+++ archive/wbtc-fix/v0.0.4-64-g58dd950/arty/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "58dd95053999cfe88b43c565d544e4011ed42fbf";
+const char* git_branch = "wbtc-fix";
+const char* git_describe = "v0.0.4-64-g58dd950";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
