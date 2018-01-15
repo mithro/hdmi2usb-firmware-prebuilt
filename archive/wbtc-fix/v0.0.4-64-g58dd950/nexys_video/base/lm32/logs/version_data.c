28e042a68e997e63c84061c6acde83c0829ed965
Tim 'mithro' Ansell <me@mith.ro>
2018-01-15 00:49:17 +0000

    Travis build #557 of wbtc-fix/v0.0.4-64-g58dd950 for PLATFORM=nexys_video TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/58dd95053999cfe88b43c565d544e4011ed42fbf

---
+++ archive/wbtc-fix/v0.0.4-64-g58dd950/nexys_video/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NEXYS_VIDEO
+#error "Version mismatch - PLATFORM_NEXYS_VIDEO not defined!"
+#endif
+const char* board = "nexys_video";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "58dd95053999cfe88b43c565d544e4011ed42fbf";
+const char* git_branch = "wbtc-fix";
+const char* git_describe = "v0.0.4-64-g58dd950";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
