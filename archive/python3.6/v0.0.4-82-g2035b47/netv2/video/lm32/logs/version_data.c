c15f28d3ef474ca2552b60468a09b1fe90f6e5af
TimVideos.us Robot <robot@timvideos.us>
2018-01-17 20:41:03 -0800

    Travis build #584 of python3.6/v0.0.4-82-g2035b47 for PLATFORM=netv2 TARGET=video CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/2035b47b6846e134630472b90c0a89472daae29a

---
+++ archive/python3.6/v0.0.4-82-g2035b47/netv2/video/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NETV2
+#error "Version mismatch - PLATFORM_NETV2 not defined!"
+#endif
+const char* board = "netv2";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "2035b47b6846e134630472b90c0a89472daae29a";
+const char* git_branch = "python3.6";
+const char* git_describe = "v0.0.4-82-g2035b47";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
