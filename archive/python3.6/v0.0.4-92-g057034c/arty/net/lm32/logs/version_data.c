ab307ad8293ab56e9937840fcd130408bf6f48cb
TimVideos.us Robot <robot@timvideos.us>
2018-01-17 22:30:50 -0800

    Travis build #589 of python3.6/v0.0.4-92-g057034c for PLATFORM=arty TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/057034c59f68ab4cbc706734a718a04f5c387346

---
+++ archive/python3.6/v0.0.4-92-g057034c/arty/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "057034c59f68ab4cbc706734a718a04f5c387346";
+const char* git_branch = "python3.6";
+const char* git_describe = "v0.0.4-92-g057034c";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
