9a253ab3ef9dde50d1c4e72b3b3fa88b032ee6fb
TimVideos.us Robot <robot@timvideos.us>
2018-01-17 18:07:38 -0800

    Travis build #582 of prebuilt-subversion/v0.0.4-76-gf95eaf4 for PLATFORM=arty TARGET=net CPU=or1k.linux FIRMWARE=linux
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/f95eaf40fd9e7b91488bbadf8061636916b8e4db

---
+++ archive/prebuilt-subversion/v0.0.4-76-gf95eaf4/arty/net/or1k.linux/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "f95eaf40fd9e7b91488bbadf8061636916b8e4db";
+const char* git_branch = "prebuilt-subversion";
+const char* git_describe = "v0.0.4-76-gf95eaf4";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
