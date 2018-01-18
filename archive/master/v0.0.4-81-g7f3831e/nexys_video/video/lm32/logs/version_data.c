adc57b7bd53063558d8fb37e8dcd9d2559f68822
TimVideos.us Robot <robot@timvideos.us>
2018-01-17 19:31:54 -0800

    Travis build #583 of master/v0.0.4-81-g7f3831e for PLATFORM=nexys_video TARGET=video CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/7f3831edb0ce0d65b1dc1787520133442ff70866

---
+++ archive/master/v0.0.4-81-g7f3831e/nexys_video/video/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NEXYS_VIDEO
+#error "Version mismatch - PLATFORM_NEXYS_VIDEO not defined!"
+#endif
+const char* board = "nexys_video";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "7f3831edb0ce0d65b1dc1787520133442ff70866";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-81-g7f3831e";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
