1b7ca4f279ab38068bcae246bf26da1c18d09005
TimVideos.us Robot <robot@timvideos.us>
2018-01-17 19:46:06 -0800

    Travis build #584 of python3.6/v0.0.4-82-g2035b47 for PLATFORM=arty TARGET=base CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/2035b47b6846e134630472b90c0a89472daae29a

---
+++ archive/python3.6/v0.0.4-82-g2035b47/arty/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "2035b47b6846e134630472b90c0a89472daae29a";
+const char* git_branch = "python3.6";
+const char* git_describe = "v0.0.4-82-g2035b47";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
