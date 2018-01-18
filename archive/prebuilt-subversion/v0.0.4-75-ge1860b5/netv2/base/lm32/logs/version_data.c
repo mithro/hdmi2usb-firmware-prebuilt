d10039310ebd4f5ccb794e26e78d7ede54bc3bd5
TimVideos.us Robot <robot@timvideos.us>
2018-01-17 16:51:27 -0800

    Travis build #581 of prebuilt-subversion/v0.0.4-75-ge1860b5 for PLATFORM=netv2 TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/e1860b53febace37006440a4f47fc86c542d7b81

---
+++ archive/prebuilt-subversion/v0.0.4-75-ge1860b5/netv2/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NETV2
+#error "Version mismatch - PLATFORM_NETV2 not defined!"
+#endif
+const char* board = "netv2";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "e1860b53febace37006440a4f47fc86c542d7b81";
+const char* git_branch = "prebuilt-subversion";
+const char* git_describe = "v0.0.4-75-ge1860b5";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
