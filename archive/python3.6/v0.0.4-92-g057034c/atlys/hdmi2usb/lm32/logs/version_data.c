77b0ed9c110e50855356ae13f2e88218b88065dd
TimVideos.us Robot <robot@timvideos.us>
2018-01-18 00:47:04 -0800

    Travis build #589 of python3.6/v0.0.4-92-g057034c for PLATFORM=atlys TARGET=hdmi2usb CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/057034c59f68ab4cbc706734a718a04f5c387346

---
+++ archive/python3.6/v0.0.4-92-g057034c/atlys/hdmi2usb/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_HDMI2USB
+#error "Version mismatch - TARGET_HDMI2USB not defined!"
+#endif
+const char* target = "hdmi2usb";
+
+const char* git_commit = "057034c59f68ab4cbc706734a718a04f5c387346";
+const char* git_branch = "python3.6";
+const char* git_describe = "v0.0.4-92-g057034c";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
