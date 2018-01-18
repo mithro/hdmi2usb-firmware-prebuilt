e2dd6bf8e4b95fa1f74e3764fd816d64a18c3fcf
TimVideos.us Robot <robot@timvideos.us>
2018-01-17 23:05:06 -0800

    Travis build #589 of python3.6/v0.0.4-92-g057034c for PLATFORM=opsis TARGET=net CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/057034c59f68ab4cbc706734a718a04f5c387346

---
+++ archive/python3.6/v0.0.4-92-g057034c/opsis/net/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "057034c59f68ab4cbc706734a718a04f5c387346";
+const char* git_branch = "python3.6";
+const char* git_describe = "v0.0.4-92-g057034c";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
