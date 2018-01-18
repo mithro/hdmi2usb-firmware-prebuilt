a02a96a1c1ec9c696447c5a65a146d5a60bc7447
TimVideos.us Robot <robot@timvideos.us>
2018-01-17 21:43:22 -0800

    Travis build #586 of fix-colors/v0.0.4-83-gc9e2012 for PLATFORM=atlys TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/c9e2012f72792401569a96a5690a6aabb8a2a1f0

---
+++ archive/fix-colors/v0.0.4-83-gc9e2012/atlys/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "c9e2012f72792401569a96a5690a6aabb8a2a1f0";
+const char* git_branch = "fix-colors";
+const char* git_describe = "v0.0.4-83-gc9e2012";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
