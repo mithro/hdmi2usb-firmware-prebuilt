e5cde34303f35e9a6b5f04365a2c1b1415775e50
TimVideos.us Robot <robot@timvideos.us>
2018-01-17 17:46:24 -0800

    Travis build #582 of prebuilt-subversion/v0.0.4-76-gf95eaf4 for PLATFORM=nexys_video TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/f95eaf40fd9e7b91488bbadf8061636916b8e4db

---
+++ archive/prebuilt-subversion/v0.0.4-76-gf95eaf4/nexys_video/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NEXYS_VIDEO
+#error "Version mismatch - PLATFORM_NEXYS_VIDEO not defined!"
+#endif
+const char* board = "nexys_video";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "f95eaf40fd9e7b91488bbadf8061636916b8e4db";
+const char* git_branch = "prebuilt-subversion";
+const char* git_describe = "v0.0.4-76-gf95eaf4";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
