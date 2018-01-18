275f2423ede5a8d208f4d43272e74f6f0c686baf
TimVideos.us Robot <robot@timvideos.us>
2018-01-17 16:50:08 -0800

    Travis build #581 of prebuilt-subversion/v0.0.4-75-ge1860b5 for PLATFORM=arty TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/e1860b53febace37006440a4f47fc86c542d7b81

---
+++ archive/prebuilt-subversion/v0.0.4-75-ge1860b5/arty/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "e1860b53febace37006440a4f47fc86c542d7b81";
+const char* git_branch = "prebuilt-subversion";
+const char* git_describe = "v0.0.4-75-ge1860b5";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
