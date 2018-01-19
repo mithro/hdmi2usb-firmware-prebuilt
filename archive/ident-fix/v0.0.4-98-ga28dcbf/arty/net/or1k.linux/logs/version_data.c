028ecbfa6023a5103277ec3bf0755b779e482f69
TimVideos.us Robot <robot@timvideos.us>
2018-01-19 07:01:29 -0800

    Travis build #592 of ident-fix/v0.0.4-98-ga28dcbf for PLATFORM=arty TARGET=net CPU=or1k.linux FIRMWARE=linux
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/a28dcbf833fa5acea630cad29b43cd321d2c9ede

---
+++ archive/ident-fix/v0.0.4-98-ga28dcbf/arty/net/or1k.linux/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "a28dcbf833fa5acea630cad29b43cd321d2c9ede";
+const char* git_branch = "ident-fix";
+const char* git_describe = "v0.0.4-98-ga28dcbf";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
