f57b3989c155a5f3d452cd8faa2df56584f04bc8
TimVideos.us Robot <robot@timvideos.us>
2018-01-19 00:27:35 -0800

    Travis build #590 of ident-fix/v0.0.4-92-ge9ac228 for PLATFORM=nexys_video TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/e9ac228b3a30e9130290382d7dedc7403401cf6a

---
+++ archive/ident-fix/v0.0.4-92-ge9ac228/nexys_video/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NEXYS_VIDEO
+#error "Version mismatch - PLATFORM_NEXYS_VIDEO not defined!"
+#endif
+const char* board = "nexys_video";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "e9ac228b3a30e9130290382d7dedc7403401cf6a";
+const char* git_branch = "ident-fix";
+const char* git_describe = "v0.0.4-92-ge9ac228";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
