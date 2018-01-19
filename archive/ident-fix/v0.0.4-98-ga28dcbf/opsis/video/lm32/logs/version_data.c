894d3a16c009307830a55565cb062b288ea487cb
TimVideos.us Robot <robot@timvideos.us>
2018-01-19 07:28:16 -0800

    Travis build #592 of ident-fix/v0.0.4-98-ga28dcbf for PLATFORM=opsis TARGET=video CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/a28dcbf833fa5acea630cad29b43cd321d2c9ede

---
+++ archive/ident-fix/v0.0.4-98-ga28dcbf/opsis/video/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "a28dcbf833fa5acea630cad29b43cd321d2c9ede";
+const char* git_branch = "ident-fix";
+const char* git_describe = "v0.0.4-98-ga28dcbf";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
