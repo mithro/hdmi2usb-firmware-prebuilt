1eac48ba994073b90a6875898c835953a75fa8fe
TimVideos.us Robot <robot@timvideos.us>
2018-01-19 00:41:50 -0800

    Travis build #590 of ident-fix/v0.0.4-92-ge9ac228 for PLATFORM=atlys TARGET=net CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/e9ac228b3a30e9130290382d7dedc7403401cf6a

---
+++ archive/ident-fix/v0.0.4-92-ge9ac228/atlys/net/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "e9ac228b3a30e9130290382d7dedc7403401cf6a";
+const char* git_branch = "ident-fix";
+const char* git_describe = "v0.0.4-92-ge9ac228";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
