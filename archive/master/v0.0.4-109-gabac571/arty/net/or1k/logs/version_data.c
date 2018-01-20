9b7607f58a0ddc6448843aef5e6561cdc31ee1c8
TimVideos.us Robot <robot@timvideos.us>
2018-01-20 04:38:43 -0800

    Travis build #594 of master/v0.0.4-109-gabac571 for PLATFORM=arty TARGET=net CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/abac5717215ced5f1c80d341580f4c38c2efbdb2

---
+++ archive/master/v0.0.4-109-gabac571/arty/net/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "abac5717215ced5f1c80d341580f4c38c2efbdb2";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-109-gabac571";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
