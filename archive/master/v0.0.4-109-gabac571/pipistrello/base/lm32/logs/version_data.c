823e0a45bfed2e524328d43256fabd34205b23a8
TimVideos.us Robot <robot@timvideos.us>
2018-01-20 04:19:08 -0800

    Travis build #594 of master/v0.0.4-109-gabac571 for PLATFORM=pipistrello TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/abac5717215ced5f1c80d341580f4c38c2efbdb2

---
+++ archive/master/v0.0.4-109-gabac571/pipistrello/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_PIPISTRELLO
+#error "Version mismatch - PLATFORM_PIPISTRELLO not defined!"
+#endif
+const char* board = "pipistrello";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "abac5717215ced5f1c80d341580f4c38c2efbdb2";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-109-gabac571";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
