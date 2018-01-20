d0ef52b0711bdb9acd7a9c591494479369bb5f55
TimVideos.us Robot <robot@timvideos.us>
2018-01-20 04:01:56 -0800

    Travis build #594 of master/v0.0.4-109-gabac571 for PLATFORM=mimasv2 TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/abac5717215ced5f1c80d341580f4c38c2efbdb2

---
+++ archive/master/v0.0.4-109-gabac571/mimasv2/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MIMASV2
+#error "Version mismatch - PLATFORM_MIMASV2 not defined!"
+#endif
+const char* board = "mimasv2";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "abac5717215ced5f1c80d341580f4c38c2efbdb2";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-109-gabac571";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
