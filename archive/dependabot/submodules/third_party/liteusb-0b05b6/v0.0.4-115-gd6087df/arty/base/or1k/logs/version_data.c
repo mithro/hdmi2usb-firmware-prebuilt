d5cb70ffda2090c89da3ba92aeb7d726a2aa68d2
TimVideos.us Robot <robot@timvideos.us>
2018-01-22 13:45:21 -0800

    Travis build #599 of dependabot/submodules/third_party/liteusb-0b05b6/v0.0.4-115-gd6087df for PLATFORM=arty TARGET=base CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/d6087df17732f1e605d18f7503176a58a6785289

---
+++ archive/dependabot/submodules/third_party/liteusb-0b05b6/v0.0.4-115-gd6087df/arty/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "d6087df17732f1e605d18f7503176a58a6785289";
+const char* git_branch = "dependabot/submodules/third_party/liteusb-0b05b6";
+const char* git_describe = "v0.0.4-115-gd6087df";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
