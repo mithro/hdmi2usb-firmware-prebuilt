e491d44b5d114c8d7602938a798d35ef9eb3f005
TimVideos.us Robot <robot@timvideos.us>
2018-01-22 14:53:12 -0800

    Travis build #600 of dependabot/submodules/third_party/litescope-aa44da/v0.0.4-115-gdee4649 for PLATFORM=pipistrello TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/dee4649c16d931eac197b73cba1f7f7380db5835

---
+++ archive/dependabot/submodules/third_party/litescope-aa44da/v0.0.4-115-gdee4649/pipistrello/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_PIPISTRELLO
+#error "Version mismatch - PLATFORM_PIPISTRELLO not defined!"
+#endif
+const char* board = "pipistrello";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "dee4649c16d931eac197b73cba1f7f7380db5835";
+const char* git_branch = "dependabot/submodules/third_party/litescope-aa44da";
+const char* git_describe = "v0.0.4-115-gdee4649";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
