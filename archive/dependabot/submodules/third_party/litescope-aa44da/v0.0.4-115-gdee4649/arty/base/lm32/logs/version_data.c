6e7464c0847602a7eaea3ca7aff0091fda5f4008
TimVideos.us Robot <robot@timvideos.us>
2018-01-22 14:11:57 -0800

    Travis build #600 of dependabot/submodules/third_party/litescope-aa44da/v0.0.4-115-gdee4649 for PLATFORM=arty TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/dee4649c16d931eac197b73cba1f7f7380db5835

---
+++ archive/dependabot/submodules/third_party/litescope-aa44da/v0.0.4-115-gdee4649/arty/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "dee4649c16d931eac197b73cba1f7f7380db5835";
+const char* git_branch = "dependabot/submodules/third_party/litescope-aa44da";
+const char* git_describe = "v0.0.4-115-gdee4649";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
