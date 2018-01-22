e425ac54eb4340c1fab782ea2fb13b703a9ab1d7
TimVideos.us Robot <robot@timvideos.us>
2018-01-22 14:41:16 -0800

    Travis build #600 of dependabot/submodules/third_party/litescope-aa44da/v0.0.4-115-gdee4649 for PLATFORM=netv2 TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/dee4649c16d931eac197b73cba1f7f7380db5835

---
+++ archive/dependabot/submodules/third_party/litescope-aa44da/v0.0.4-115-gdee4649/netv2/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NETV2
+#error "Version mismatch - PLATFORM_NETV2 not defined!"
+#endif
+const char* board = "netv2";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "dee4649c16d931eac197b73cba1f7f7380db5835";
+const char* git_branch = "dependabot/submodules/third_party/litescope-aa44da";
+const char* git_describe = "v0.0.4-115-gdee4649";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
