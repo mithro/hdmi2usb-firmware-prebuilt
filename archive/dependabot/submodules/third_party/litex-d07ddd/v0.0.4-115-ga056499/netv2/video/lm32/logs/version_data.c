37c536b8f7ee5e7a7894b170a70a522ff3348579
TimVideos.us Robot <robot@timvideos.us>
2018-01-22 17:07:25 -0800

    Travis build #601 of dependabot/submodules/third_party/litex-d07ddd/v0.0.4-115-ga056499 for PLATFORM=netv2 TARGET=video CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/a05649960f772ad997cd4a7d4e83e4f25457e842

---
+++ archive/dependabot/submodules/third_party/litex-d07ddd/v0.0.4-115-ga056499/netv2/video/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NETV2
+#error "Version mismatch - PLATFORM_NETV2 not defined!"
+#endif
+const char* board = "netv2";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "a05649960f772ad997cd4a7d4e83e4f25457e842";
+const char* git_branch = "dependabot/submodules/third_party/litex-d07ddd";
+const char* git_describe = "v0.0.4-115-ga056499";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
