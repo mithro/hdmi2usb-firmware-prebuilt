51f47e7074618e68f9cd23b60a275e92b94c1c13
TimVideos.us Robot <robot@timvideos.us>
2018-01-22 16:41:25 -0800

    Travis build #601 of dependabot/submodules/third_party/litex-d07ddd/v0.0.4-115-ga056499 for PLATFORM=pipistrello TARGET=base CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/a05649960f772ad997cd4a7d4e83e4f25457e842

---
+++ archive/dependabot/submodules/third_party/litex-d07ddd/v0.0.4-115-ga056499/pipistrello/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_PIPISTRELLO
+#error "Version mismatch - PLATFORM_PIPISTRELLO not defined!"
+#endif
+const char* board = "pipistrello";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "a05649960f772ad997cd4a7d4e83e4f25457e842";
+const char* git_branch = "dependabot/submodules/third_party/litex-d07ddd";
+const char* git_describe = "v0.0.4-115-ga056499";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
