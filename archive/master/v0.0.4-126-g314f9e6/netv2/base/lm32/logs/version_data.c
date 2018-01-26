6cdce18d874c3372203256f282f532fe3d08a78c
TimVideos.us Robot <robot@timvideos.us>
2018-01-25 17:32:07 -0800

    Travis build #604 of master/v0.0.4-126-g314f9e6 for PLATFORM=netv2 TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/314f9e6591aec4606da11751910208591e1e4505

---
+++ archive/master/v0.0.4-126-g314f9e6/netv2/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NETV2
+#error "Version mismatch - PLATFORM_NETV2 not defined!"
+#endif
+const char* board = "netv2";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "314f9e6591aec4606da11751910208591e1e4505";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-126-g314f9e6";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
