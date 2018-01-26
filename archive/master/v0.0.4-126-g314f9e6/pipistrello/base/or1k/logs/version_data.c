85835f3ec13a59fbca16a25ef063ee36688fd843
TimVideos.us Robot <robot@timvideos.us>
2018-01-25 18:11:13 -0800

    Travis build #604 of master/v0.0.4-126-g314f9e6 for PLATFORM=pipistrello TARGET=base CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/314f9e6591aec4606da11751910208591e1e4505

---
+++ archive/master/v0.0.4-126-g314f9e6/pipistrello/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_PIPISTRELLO
+#error "Version mismatch - PLATFORM_PIPISTRELLO not defined!"
+#endif
+const char* board = "pipistrello";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "314f9e6591aec4606da11751910208591e1e4505";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-126-g314f9e6";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
