c85aecf4f9772d8ae6a086e2eb9cfd9a789dc3c2
TimVideos.us Robot <robot@timvideos.us>
2018-01-29 15:24:50 -0800

    Travis build #607 of dependabot/submodules/third_party/litex-2ecd1b/v0.0.4-130-gf2b82c4 for PLATFORM=mimasv2 TARGET=base CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/f2b82c459a1d89dd358fe49c35b5bdaf1f64933b

---
+++ archive/dependabot/submodules/third_party/litex-2ecd1b/v0.0.4-130-gf2b82c4/mimasv2/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MIMASV2
+#error "Version mismatch - PLATFORM_MIMASV2 not defined!"
+#endif
+const char* board = "mimasv2";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "f2b82c459a1d89dd358fe49c35b5bdaf1f64933b";
+const char* git_branch = "dependabot/submodules/third_party/litex-2ecd1b";
+const char* git_describe = "v0.0.4-130-gf2b82c4";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
