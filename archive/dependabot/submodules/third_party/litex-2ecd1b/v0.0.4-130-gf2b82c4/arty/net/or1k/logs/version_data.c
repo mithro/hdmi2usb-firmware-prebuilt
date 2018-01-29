ea3d4e3740db5e98f18da78446e373a45b7083e3
TimVideos.us Robot <robot@timvideos.us>
2018-01-29 15:14:10 -0800

    Travis build #607 of dependabot/submodules/third_party/litex-2ecd1b/v0.0.4-130-gf2b82c4 for PLATFORM=arty TARGET=net CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/f2b82c459a1d89dd358fe49c35b5bdaf1f64933b

---
+++ archive/dependabot/submodules/third_party/litex-2ecd1b/v0.0.4-130-gf2b82c4/arty/net/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "f2b82c459a1d89dd358fe49c35b5bdaf1f64933b";
+const char* git_branch = "dependabot/submodules/third_party/litex-2ecd1b";
+const char* git_describe = "v0.0.4-130-gf2b82c4";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
