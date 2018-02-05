64814ac916b71aff78b5ae46c3bb50c371198e85
TimVideos.us Robot <robot@timvideos.us>
2018-02-05 13:54:32 -0800

    Travis build #611 of dependabot/submodules/third_party/litedram-ec9ad2/v0.0.4-130-g898ec03 for PLATFORM=opsis TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/898ec035454c51a83814ba9e76ccdfe326408ae4

---
+++ archive/dependabot/submodules/third_party/litedram-ec9ad2/v0.0.4-130-g898ec03/opsis/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "898ec035454c51a83814ba9e76ccdfe326408ae4";
+const char* git_branch = "dependabot/submodules/third_party/litedram-ec9ad2";
+const char* git_describe = "v0.0.4-130-g898ec03";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
