a2cf08932e7112a5ed78dc8cb3f19150d9fcb0a5
TimVideos.us Robot <robot@timvideos.us>
2018-02-05 12:51:29 -0800

    Travis build #610 of dependabot/submodules/third_party/litepcie-98a2c7/v0.0.4-130-g9c3122b for PLATFORM=opsis TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/9c3122bc8de7b010e110c5172e49a2145cd462cf

---
+++ archive/dependabot/submodules/third_party/litepcie-98a2c7/v0.0.4-130-g9c3122b/opsis/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "9c3122bc8de7b010e110c5172e49a2145cd462cf";
+const char* git_branch = "dependabot/submodules/third_party/litepcie-98a2c7";
+const char* git_describe = "v0.0.4-130-g9c3122b";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
