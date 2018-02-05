3bae36cbb1f038b88ad53c5540daada4a7957e1e
TimVideos.us Robot <robot@timvideos.us>
2018-02-05 14:47:38 -0800

    Travis build #611 of dependabot/submodules/third_party/litedram-ec9ad2/v0.0.4-130-g898ec03 for PLATFORM=netv2 TARGET=video CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/898ec035454c51a83814ba9e76ccdfe326408ae4

---
+++ archive/dependabot/submodules/third_party/litedram-ec9ad2/v0.0.4-130-g898ec03/netv2/video/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NETV2
+#error "Version mismatch - PLATFORM_NETV2 not defined!"
+#endif
+const char* board = "netv2";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "898ec035454c51a83814ba9e76ccdfe326408ae4";
+const char* git_branch = "dependabot/submodules/third_party/litedram-ec9ad2";
+const char* git_describe = "v0.0.4-130-g898ec03";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
