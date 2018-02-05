222c0aa15e869c0c15b761df6620163f90e9f7c1
TimVideos.us Robot <robot@timvideos.us>
2018-02-05 13:20:14 -0800

    Travis build #611 of dependabot/submodules/third_party/litedram-ec9ad2/v0.0.4-130-g898ec03 for PLATFORM=arty TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/898ec035454c51a83814ba9e76ccdfe326408ae4

---
+++ archive/dependabot/submodules/third_party/litedram-ec9ad2/v0.0.4-130-g898ec03/arty/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "898ec035454c51a83814ba9e76ccdfe326408ae4";
+const char* git_branch = "dependabot/submodules/third_party/litedram-ec9ad2";
+const char* git_describe = "v0.0.4-130-g898ec03";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
