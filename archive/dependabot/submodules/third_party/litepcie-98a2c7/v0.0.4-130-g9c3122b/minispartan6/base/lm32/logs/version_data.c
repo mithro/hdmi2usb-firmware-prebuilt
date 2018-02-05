3e904aff85684113d563dceeaf7a077f2654f29f
TimVideos.us Robot <robot@timvideos.us>
2018-02-05 12:24:21 -0800

    Travis build #610 of dependabot/submodules/third_party/litepcie-98a2c7/v0.0.4-130-g9c3122b for PLATFORM=minispartan6 TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/9c3122bc8de7b010e110c5172e49a2145cd462cf

---
+++ archive/dependabot/submodules/third_party/litepcie-98a2c7/v0.0.4-130-g9c3122b/minispartan6/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MINISPARTAN6
+#error "Version mismatch - PLATFORM_MINISPARTAN6 not defined!"
+#endif
+const char* board = "minispartan6";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "9c3122bc8de7b010e110c5172e49a2145cd462cf";
+const char* git_branch = "dependabot/submodules/third_party/litepcie-98a2c7";
+const char* git_describe = "v0.0.4-130-g9c3122b";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
