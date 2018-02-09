9dbdd7b7c4d08ae21b4ede5394874ab4f7665861
TimVideos.us Robot <robot@timvideos.us>
2018-02-08 20:57:03 -0800

    Travis build #615 of master/v0.0.4-140-g6f75670 for PLATFORM=arty TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/6f75670d83a55d6540249df2b2a0b4e1dfb7f508

---
+++ archive/master/v0.0.4-140-g6f75670/arty/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "6f75670d83a55d6540249df2b2a0b4e1dfb7f508";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-140-g6f75670";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
