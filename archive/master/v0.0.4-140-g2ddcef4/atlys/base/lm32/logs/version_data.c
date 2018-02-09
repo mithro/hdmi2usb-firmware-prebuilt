e5973bc6d45eef5645907cb2a6e8b3331c47b875
TimVideos.us Robot <robot@timvideos.us>
2018-02-08 21:09:03 -0800

    Travis build #616 of master/v0.0.4-140-g2ddcef4 for PLATFORM=atlys TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/2ddcef4c25acc0d6759c87dd6219fab509d149b3

---
+++ archive/master/v0.0.4-140-g2ddcef4/atlys/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "2ddcef4c25acc0d6759c87dd6219fab509d149b3";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-140-g2ddcef4";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
