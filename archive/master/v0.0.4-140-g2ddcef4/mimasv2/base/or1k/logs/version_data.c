0b034f0a675891ff17bdcfe2eb05c8af7b6143c6
TimVideos.us Robot <robot@timvideos.us>
2018-02-08 22:30:31 -0800

    Travis build #616 of master/v0.0.4-140-g2ddcef4 for PLATFORM=mimasv2 TARGET=base CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/2ddcef4c25acc0d6759c87dd6219fab509d149b3

---
+++ archive/master/v0.0.4-140-g2ddcef4/mimasv2/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MIMASV2
+#error "Version mismatch - PLATFORM_MIMASV2 not defined!"
+#endif
+const char* board = "mimasv2";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "2ddcef4c25acc0d6759c87dd6219fab509d149b3";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-140-g2ddcef4";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
