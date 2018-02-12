3b44e1b3b254ed4ac095f1e2cb49cb220629e2fd
TimVideos.us Robot <robot@timvideos.us>
2018-02-12 13:05:22 -0800

    Travis build #622 of dependabot/submodules/third_party/litevideo-a7e289/v0.0.4-145-gf1eecfd for PLATFORM=nexys_video TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/f1eecfdf655a0debfcbb903f772c8afa3e64e446

---
+++ archive/dependabot/submodules/third_party/litevideo-a7e289/v0.0.4-145-gf1eecfd/nexys_video/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NEXYS_VIDEO
+#error "Version mismatch - PLATFORM_NEXYS_VIDEO not defined!"
+#endif
+const char* board = "nexys_video";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "f1eecfdf655a0debfcbb903f772c8afa3e64e446";
+const char* git_branch = "dependabot/submodules/third_party/litevideo-a7e289";
+const char* git_describe = "v0.0.4-145-gf1eecfd";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
---
+++ archive/dependabot/submodules/third_party/litevideo-a7e289/v0.0.4-145-gf1eecfd/nexys_video/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NEXYS_VIDEO
+#error "Version mismatch - PLATFORM_NEXYS_VIDEO not defined!"
+#endif
+const char* board = "nexys_video";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "f1eecfdf655a0debfcbb903f772c8afa3e64e446";
+const char* git_branch = "dependabot/submodules/third_party/litevideo-a7e289";
+const char* git_describe = "v0.0.4-145-gf1eecfd";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
