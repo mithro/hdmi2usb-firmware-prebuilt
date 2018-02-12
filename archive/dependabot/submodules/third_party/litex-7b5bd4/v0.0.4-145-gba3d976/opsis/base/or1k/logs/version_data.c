92022ab7add19642d8193839682464a961f2d7f7
TimVideos.us Robot <robot@timvideos.us>
2018-02-12 11:57:42 -0800

    Travis build #621 of dependabot/submodules/third_party/litex-7b5bd4/v0.0.4-145-gba3d976 for PLATFORM=opsis TARGET=base CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/ba3d97663761e0eb5bbfdeee3df1128d81d07309

---
+++ archive/dependabot/submodules/third_party/litex-7b5bd4/v0.0.4-145-gba3d976/opsis/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "ba3d97663761e0eb5bbfdeee3df1128d81d07309";
+const char* git_branch = "dependabot/submodules/third_party/litex-7b5bd4";
+const char* git_describe = "v0.0.4-145-gba3d976";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
