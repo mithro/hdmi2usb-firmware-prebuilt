ef288a57c678192848bafe7fd55984cdce7fe94d
TimVideos.us Robot <robot@timvideos.us>
2018-02-12 12:39:52 -0800

    Travis build #622 of dependabot/submodules/third_party/litevideo-a7e289/v0.0.4-145-gf1eecfd for PLATFORM=atlys TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/f1eecfdf655a0debfcbb903f772c8afa3e64e446

---
+++ archive/dependabot/submodules/third_party/litevideo-a7e289/v0.0.4-145-gf1eecfd/atlys/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "f1eecfdf655a0debfcbb903f772c8afa3e64e446";
+const char* git_branch = "dependabot/submodules/third_party/litevideo-a7e289";
+const char* git_describe = "v0.0.4-145-gf1eecfd";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
