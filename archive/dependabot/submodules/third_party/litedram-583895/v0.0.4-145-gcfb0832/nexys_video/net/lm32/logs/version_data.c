783fe4b6c81e4047da982fb7d240583f23a97786
TimVideos.us Robot <robot@timvideos.us>
2018-02-12 15:24:49 -0800

    Travis build #623 of dependabot/submodules/third_party/litedram-583895/v0.0.4-145-gcfb0832 for PLATFORM=nexys_video TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/cfb0832e2b1d8d0dff62ff386eb89f4fa007b7eb

---
+++ archive/dependabot/submodules/third_party/litedram-583895/v0.0.4-145-gcfb0832/nexys_video/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NEXYS_VIDEO
+#error "Version mismatch - PLATFORM_NEXYS_VIDEO not defined!"
+#endif
+const char* board = "nexys_video";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "cfb0832e2b1d8d0dff62ff386eb89f4fa007b7eb";
+const char* git_branch = "dependabot/submodules/third_party/litedram-583895";
+const char* git_describe = "v0.0.4-145-gcfb0832";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
