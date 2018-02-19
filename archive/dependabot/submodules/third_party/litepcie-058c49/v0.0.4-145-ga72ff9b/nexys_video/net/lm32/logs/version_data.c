c7ec1370794f0d9f65f8792e7831e37dfd9b2c2d
TimVideos.us Robot <robot@timvideos.us>
2018-02-19 14:31:55 -0800

    Travis build #626 of dependabot/submodules/third_party/litepcie-058c49/v0.0.4-145-ga72ff9b for PLATFORM=nexys_video TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/a72ff9ba20246a11a785ac81b862a40cc2068378

---
+++ archive/dependabot/submodules/third_party/litepcie-058c49/v0.0.4-145-ga72ff9b/nexys_video/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NEXYS_VIDEO
+#error "Version mismatch - PLATFORM_NEXYS_VIDEO not defined!"
+#endif
+const char* board = "nexys_video";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "a72ff9ba20246a11a785ac81b862a40cc2068378";
+const char* git_branch = "dependabot/submodules/third_party/litepcie-058c49";
+const char* git_describe = "v0.0.4-145-ga72ff9b";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
