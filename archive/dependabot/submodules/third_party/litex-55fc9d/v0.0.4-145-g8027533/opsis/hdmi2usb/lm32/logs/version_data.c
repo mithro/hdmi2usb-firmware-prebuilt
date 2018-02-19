4481609ec0871955d4b85f13ed296d1277ebcbd6
TimVideos.us Robot <robot@timvideos.us>
2018-02-19 13:22:01 -0800

    Travis build #624 of dependabot/submodules/third_party/litex-55fc9d/v0.0.4-145-g8027533 for PLATFORM=opsis TARGET=hdmi2usb CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/80275332774cd2e8bed375b8422cdce464a21933

---
+++ archive/dependabot/submodules/third_party/litex-55fc9d/v0.0.4-145-g8027533/opsis/hdmi2usb/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_HDMI2USB
+#error "Version mismatch - TARGET_HDMI2USB not defined!"
+#endif
+const char* target = "hdmi2usb";
+
+const char* git_commit = "80275332774cd2e8bed375b8422cdce464a21933";
+const char* git_branch = "dependabot/submodules/third_party/litex-55fc9d";
+const char* git_describe = "v0.0.4-145-g8027533";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
