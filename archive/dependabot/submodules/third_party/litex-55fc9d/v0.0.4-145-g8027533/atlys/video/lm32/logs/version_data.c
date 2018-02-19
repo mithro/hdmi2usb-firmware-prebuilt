079d6c0b651fdf383416123cb86485b27c870c22
TimVideos.us Robot <robot@timvideos.us>
2018-02-19 12:38:55 -0800

    Travis build #624 of dependabot/submodules/third_party/litex-55fc9d/v0.0.4-145-g8027533 for PLATFORM=atlys TARGET=video CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/80275332774cd2e8bed375b8422cdce464a21933

---
+++ archive/dependabot/submodules/third_party/litex-55fc9d/v0.0.4-145-g8027533/atlys/video/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "80275332774cd2e8bed375b8422cdce464a21933";
+const char* git_branch = "dependabot/submodules/third_party/litex-55fc9d";
+const char* git_describe = "v0.0.4-145-g8027533";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
