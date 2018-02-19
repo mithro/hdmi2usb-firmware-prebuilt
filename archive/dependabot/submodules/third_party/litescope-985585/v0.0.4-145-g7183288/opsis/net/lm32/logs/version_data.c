af5431d599c3ca0b06073adc8e19d85b95da4cc2
TimVideos.us Robot <robot@timvideos.us>
2018-02-19 13:01:14 -0800

    Travis build #625 of dependabot/submodules/third_party/litescope-985585/v0.0.4-145-g7183288 for PLATFORM=opsis TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/718328862a38d5ee4a152d4bf1797136b5372a72

---
+++ archive/dependabot/submodules/third_party/litescope-985585/v0.0.4-145-g7183288/opsis/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "718328862a38d5ee4a152d4bf1797136b5372a72";
+const char* git_branch = "dependabot/submodules/third_party/litescope-985585";
+const char* git_describe = "v0.0.4-145-g7183288";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
