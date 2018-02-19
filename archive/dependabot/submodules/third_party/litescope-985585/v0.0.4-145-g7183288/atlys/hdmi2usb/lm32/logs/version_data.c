0e9cb072403fc4d2c5aab70b0589c43328567a4c
TimVideos.us Robot <robot@timvideos.us>
2018-02-19 14:38:48 -0800

    Travis build #625 of dependabot/submodules/third_party/litescope-985585/v0.0.4-145-g7183288 for PLATFORM=atlys TARGET=hdmi2usb CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/718328862a38d5ee4a152d4bf1797136b5372a72

---
+++ archive/dependabot/submodules/third_party/litescope-985585/v0.0.4-145-g7183288/atlys/hdmi2usb/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_HDMI2USB
+#error "Version mismatch - TARGET_HDMI2USB not defined!"
+#endif
+const char* target = "hdmi2usb";
+
+const char* git_commit = "718328862a38d5ee4a152d4bf1797136b5372a72";
+const char* git_branch = "dependabot/submodules/third_party/litescope-985585";
+const char* git_describe = "v0.0.4-145-g7183288";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
