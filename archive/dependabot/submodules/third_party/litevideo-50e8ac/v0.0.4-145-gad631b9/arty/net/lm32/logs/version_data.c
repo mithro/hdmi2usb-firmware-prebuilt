c435f7ed10c4b8534fd46484c3df6297b2d27670
TimVideos.us Robot <robot@timvideos.us>
2018-02-19 15:15:11 -0800

    Travis build #627 of dependabot/submodules/third_party/litevideo-50e8ac/v0.0.4-145-gad631b9 for PLATFORM=arty TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/ad631b9dea936128534499b4b8f0384d428ed774

---
+++ archive/dependabot/submodules/third_party/litevideo-50e8ac/v0.0.4-145-gad631b9/arty/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "ad631b9dea936128534499b4b8f0384d428ed774";
+const char* git_branch = "dependabot/submodules/third_party/litevideo-50e8ac";
+const char* git_describe = "v0.0.4-145-gad631b9";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
