d7d1c526444d9514d50e7814c599e16b492d6c0f
TimVideos.us Robot <robot@timvideos.us>
2018-02-19 14:20:46 -0800

    Travis build #626 of dependabot/submodules/third_party/litepcie-058c49/v0.0.4-145-ga72ff9b for PLATFORM=arty TARGET=base CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/a72ff9ba20246a11a785ac81b862a40cc2068378

---
+++ archive/dependabot/submodules/third_party/litepcie-058c49/v0.0.4-145-ga72ff9b/arty/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "a72ff9ba20246a11a785ac81b862a40cc2068378";
+const char* git_branch = "dependabot/submodules/third_party/litepcie-058c49";
+const char* git_describe = "v0.0.4-145-ga72ff9b";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
