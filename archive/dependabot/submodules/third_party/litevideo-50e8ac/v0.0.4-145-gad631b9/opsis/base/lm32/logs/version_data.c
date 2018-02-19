d3da85ca5abb00bbef7b3f3ea1277ce1ce1846cd
TimVideos.us Robot <robot@timvideos.us>
2018-02-19 15:37:59 -0800

    Travis build #627 of dependabot/submodules/third_party/litevideo-50e8ac/v0.0.4-145-gad631b9 for PLATFORM=opsis TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/ad631b9dea936128534499b4b8f0384d428ed774

---
+++ archive/dependabot/submodules/third_party/litevideo-50e8ac/v0.0.4-145-gad631b9/opsis/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "ad631b9dea936128534499b4b8f0384d428ed774";
+const char* git_branch = "dependabot/submodules/third_party/litevideo-50e8ac";
+const char* git_describe = "v0.0.4-145-gad631b9";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
