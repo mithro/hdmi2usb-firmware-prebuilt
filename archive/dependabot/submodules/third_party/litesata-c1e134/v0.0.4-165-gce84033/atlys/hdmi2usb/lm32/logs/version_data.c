55d994e3ac1d903ef51148212e290f36f54296f0
TimVideos.us Robot <robot@timvideos.us>
2018-02-26 13:39:04 -0800

    Travis build #630 of dependabot/submodules/third_party/litesata-c1e134/v0.0.4-165-gce84033 for PLATFORM=atlys TARGET=hdmi2usb CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/ce84033cd34675c220752855c4b78816fcefe136

---
+++ archive/dependabot/submodules/third_party/litesata-c1e134/v0.0.4-165-gce84033/atlys/hdmi2usb/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_HDMI2USB
+#error "Version mismatch - TARGET_HDMI2USB not defined!"
+#endif
+const char* target = "hdmi2usb";
+
+const char* git_commit = "ce84033cd34675c220752855c4b78816fcefe136";
+const char* git_branch = "dependabot/submodules/third_party/litesata-c1e134";
+const char* git_describe = "v0.0.4-165-gce84033";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
