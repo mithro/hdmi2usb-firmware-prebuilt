e3f8081d31446333939a7ac66d26637b9efc1c6a
TimVideos.us Robot <robot@timvideos.us>
2018-02-26 15:48:11 -0800

    Travis build #634 of dependabot/submodules/third_party/liteusb-102a75/v0.0.4-165-g22f61f2 for PLATFORM=atlys TARGET=net CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/22f61f277415d95a63db16b71c5331bbd2a48606

---
+++ archive/dependabot/submodules/third_party/liteusb-102a75/v0.0.4-165-g22f61f2/atlys/net/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "22f61f277415d95a63db16b71c5331bbd2a48606";
+const char* git_branch = "dependabot/submodules/third_party/liteusb-102a75";
+const char* git_describe = "v0.0.4-165-g22f61f2";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
