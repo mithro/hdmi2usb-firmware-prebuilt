9ae5e24a9001649cca04c9ac144e241a4ffc33da
TimVideos.us Robot <robot@timvideos.us>
2018-02-26 11:31:13 -0800

    Travis build #630 of dependabot/submodules/third_party/litesata-c1e134/v0.0.4-165-gce84033 for PLATFORM=minispartan6 TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/ce84033cd34675c220752855c4b78816fcefe136

---
+++ archive/dependabot/submodules/third_party/litesata-c1e134/v0.0.4-165-gce84033/minispartan6/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MINISPARTAN6
+#error "Version mismatch - PLATFORM_MINISPARTAN6 not defined!"
+#endif
+const char* board = "minispartan6";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "ce84033cd34675c220752855c4b78816fcefe136";
+const char* git_branch = "dependabot/submodules/third_party/litesata-c1e134";
+const char* git_describe = "v0.0.4-165-gce84033";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
