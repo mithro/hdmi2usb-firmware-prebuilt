044511e5d83cd3a76e179c4c092e761c5d243bbc
TimVideos.us Robot <robot@timvideos.us>
2018-02-26 12:51:34 -0800

    Travis build #630 of dependabot/submodules/third_party/litesata-c1e134/v0.0.4-165-gce84033 for PLATFORM=netv2 TARGET=video CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/ce84033cd34675c220752855c4b78816fcefe136

---
+++ archive/dependabot/submodules/third_party/litesata-c1e134/v0.0.4-165-gce84033/netv2/video/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NETV2
+#error "Version mismatch - PLATFORM_NETV2 not defined!"
+#endif
+const char* board = "netv2";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "ce84033cd34675c220752855c4b78816fcefe136";
+const char* git_branch = "dependabot/submodules/third_party/litesata-c1e134";
+const char* git_describe = "v0.0.4-165-gce84033";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
