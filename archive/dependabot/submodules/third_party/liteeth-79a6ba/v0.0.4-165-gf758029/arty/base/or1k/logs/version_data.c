400f7fa5547ce925b03753bcbb175bfd27c1d21a
TimVideos.us Robot <robot@timvideos.us>
2018-02-26 13:34:56 -0800

    Travis build #631 of dependabot/submodules/third_party/liteeth-79a6ba/v0.0.4-165-gf758029 for PLATFORM=arty TARGET=base CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/f75802998122454fdada2469e026ecf1ef571f82

---
+++ archive/dependabot/submodules/third_party/liteeth-79a6ba/v0.0.4-165-gf758029/arty/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "f75802998122454fdada2469e026ecf1ef571f82";
+const char* git_branch = "dependabot/submodules/third_party/liteeth-79a6ba";
+const char* git_describe = "v0.0.4-165-gf758029";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
