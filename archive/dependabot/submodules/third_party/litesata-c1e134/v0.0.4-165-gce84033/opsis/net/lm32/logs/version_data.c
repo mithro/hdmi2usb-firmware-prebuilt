b96d5ffa6c6769f9e34a8414be51096bba81c529
TimVideos.us Robot <robot@timvideos.us>
2018-02-26 11:55:41 -0800

    Travis build #630 of dependabot/submodules/third_party/litesata-c1e134/v0.0.4-165-gce84033 for PLATFORM=opsis TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/ce84033cd34675c220752855c4b78816fcefe136

---
+++ archive/dependabot/submodules/third_party/litesata-c1e134/v0.0.4-165-gce84033/opsis/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "ce84033cd34675c220752855c4b78816fcefe136";
+const char* git_branch = "dependabot/submodules/third_party/litesata-c1e134";
+const char* git_describe = "v0.0.4-165-gce84033";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
