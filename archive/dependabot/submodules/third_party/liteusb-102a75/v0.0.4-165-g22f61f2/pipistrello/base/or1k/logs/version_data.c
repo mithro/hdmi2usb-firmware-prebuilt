a80c06204231746a1cda6145b874c0e4473b108f
TimVideos.us Robot <robot@timvideos.us>
2018-02-26 15:53:33 -0800

    Travis build #634 of dependabot/submodules/third_party/liteusb-102a75/v0.0.4-165-g22f61f2 for PLATFORM=pipistrello TARGET=base CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/22f61f277415d95a63db16b71c5331bbd2a48606

---
+++ archive/dependabot/submodules/third_party/liteusb-102a75/v0.0.4-165-g22f61f2/pipistrello/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_PIPISTRELLO
+#error "Version mismatch - PLATFORM_PIPISTRELLO not defined!"
+#endif
+const char* board = "pipistrello";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "22f61f277415d95a63db16b71c5331bbd2a48606";
+const char* git_branch = "dependabot/submodules/third_party/liteusb-102a75";
+const char* git_describe = "v0.0.4-165-g22f61f2";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
