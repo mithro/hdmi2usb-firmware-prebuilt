3f8e7cf0d6bdc3e766e80e31d27418955bd081bd
TimVideos.us Robot <robot@timvideos.us>
2018-02-26 18:49:05 -0800

    Travis build #636 of dependabot/submodules/third_party/litepcie-d9b8b2/v0.0.4-165-g2e5f967 for PLATFORM=netv2 TARGET=video CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/2e5f9673967682a6dd5337b79d8fe34dd2e2dbfe

---
+++ archive/dependabot/submodules/third_party/litepcie-d9b8b2/v0.0.4-165-g2e5f967/netv2/video/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NETV2
+#error "Version mismatch - PLATFORM_NETV2 not defined!"
+#endif
+const char* board = "netv2";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "2e5f9673967682a6dd5337b79d8fe34dd2e2dbfe";
+const char* git_branch = "dependabot/submodules/third_party/litepcie-d9b8b2";
+const char* git_describe = "v0.0.4-165-g2e5f967";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
