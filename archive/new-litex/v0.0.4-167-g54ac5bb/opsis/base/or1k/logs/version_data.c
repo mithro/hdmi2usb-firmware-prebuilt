d324908771f03ea68599ab1d2e4237e37313e2fc
TimVideos.us Robot <robot@timvideos.us>
2018-03-04 15:13:46 -0800

    Travis build #638 of new-litex/v0.0.4-167-g54ac5bb for PLATFORM=opsis TARGET=base CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/54ac5bb2c44b18b5b7ce447ddc06b8de99e2954d

---
+++ archive/new-litex/v0.0.4-167-g54ac5bb/opsis/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "54ac5bb2c44b18b5b7ce447ddc06b8de99e2954d";
+const char* git_branch = "new-litex";
+const char* git_describe = "v0.0.4-167-g54ac5bb";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
