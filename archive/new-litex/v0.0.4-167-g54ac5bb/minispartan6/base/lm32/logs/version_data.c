c5c7076a2cf490367a73adf55390eb57e23c73cb
TimVideos.us Robot <robot@timvideos.us>
2018-03-04 14:36:09 -0800

    Travis build #638 of new-litex/v0.0.4-167-g54ac5bb for PLATFORM=minispartan6 TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/54ac5bb2c44b18b5b7ce447ddc06b8de99e2954d

---
+++ archive/new-litex/v0.0.4-167-g54ac5bb/minispartan6/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MINISPARTAN6
+#error "Version mismatch - PLATFORM_MINISPARTAN6 not defined!"
+#endif
+const char* board = "minispartan6";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "54ac5bb2c44b18b5b7ce447ddc06b8de99e2954d";
+const char* git_branch = "new-litex";
+const char* git_describe = "v0.0.4-167-g54ac5bb";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
