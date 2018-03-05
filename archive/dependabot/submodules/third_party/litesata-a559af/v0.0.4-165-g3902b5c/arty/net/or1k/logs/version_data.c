76e851eba5f7100fcd11f4a5d6ca16d171d3a348
TimVideos.us Robot <robot@timvideos.us>
2018-03-05 15:38:49 -0800

    Travis build #644 of dependabot/submodules/third_party/litesata-a559af/v0.0.4-165-g3902b5c for PLATFORM=arty TARGET=net CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/3902b5c197ac880356d0e541213b82f5aa2b3c7c

---
+++ archive/dependabot/submodules/third_party/litesata-a559af/v0.0.4-165-g3902b5c/arty/net/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "3902b5c197ac880356d0e541213b82f5aa2b3c7c";
+const char* git_branch = "dependabot/submodules/third_party/litesata-a559af";
+const char* git_describe = "v0.0.4-165-g3902b5c";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
