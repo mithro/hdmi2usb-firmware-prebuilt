2740a029c5015e948ccce6ce24e0fd2191816ce6
TimVideos.us Robot <robot@timvideos.us>
2018-03-05 15:12:16 -0800

    Travis build #644 of dependabot/submodules/third_party/litesata-a559af/v0.0.4-165-g3902b5c for PLATFORM=nexys_video TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/3902b5c197ac880356d0e541213b82f5aa2b3c7c

---
+++ archive/dependabot/submodules/third_party/litesata-a559af/v0.0.4-165-g3902b5c/nexys_video/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NEXYS_VIDEO
+#error "Version mismatch - PLATFORM_NEXYS_VIDEO not defined!"
+#endif
+const char* board = "nexys_video";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "3902b5c197ac880356d0e541213b82f5aa2b3c7c";
+const char* git_branch = "dependabot/submodules/third_party/litesata-a559af";
+const char* git_describe = "v0.0.4-165-g3902b5c";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
