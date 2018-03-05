5be7b3fa8371219941968b3d2236f29592139149
TimVideos.us Robot <robot@timvideos.us>
2018-03-05 15:32:16 -0800

    Travis build #644 of dependabot/submodules/third_party/litesata-a559af/v0.0.4-165-g3902b5c for PLATFORM=atlys TARGET=base CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/3902b5c197ac880356d0e541213b82f5aa2b3c7c

---
+++ archive/dependabot/submodules/third_party/litesata-a559af/v0.0.4-165-g3902b5c/atlys/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "3902b5c197ac880356d0e541213b82f5aa2b3c7c";
+const char* git_branch = "dependabot/submodules/third_party/litesata-a559af";
+const char* git_describe = "v0.0.4-165-g3902b5c";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
