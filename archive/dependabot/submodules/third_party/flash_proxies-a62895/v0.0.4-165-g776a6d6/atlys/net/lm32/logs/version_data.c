2b6ef83d69ae2eba0f5f8e8a50630a7263c814f0
TimVideos.us Robot <robot@timvideos.us>
2018-03-05 14:28:10 -0800

    Travis build #641 of dependabot/submodules/third_party/flash_proxies-a62895/v0.0.4-165-g776a6d6 for PLATFORM=atlys TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/776a6d68c6e5031551c294975eea720792aaa2ce

---
+++ archive/dependabot/submodules/third_party/flash_proxies-a62895/v0.0.4-165-g776a6d6/atlys/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "776a6d68c6e5031551c294975eea720792aaa2ce";
+const char* git_branch = "dependabot/submodules/third_party/flash_proxies-a62895";
+const char* git_describe = "v0.0.4-165-g776a6d6";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
