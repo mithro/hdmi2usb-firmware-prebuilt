bee9fa84beb98d4757ef18b8727d3c3bf91e54cb
TimVideos.us Robot <robot@timvideos.us>
2018-03-12 14:52:28 -0700

    Travis build #650 of dependabot/submodules/third_party/litepcie-8bc328/v0.0.4-165-g83ee868 for PLATFORM=atlys TARGET=video CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/83ee86839c969feb81aafd42726ea528a75447f3

---
+++ archive/dependabot/submodules/third_party/litepcie-8bc328/v0.0.4-165-g83ee868/atlys/video/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "83ee86839c969feb81aafd42726ea528a75447f3";
+const char* git_branch = "dependabot/submodules/third_party/litepcie-8bc328";
+const char* git_describe = "v0.0.4-165-g83ee868";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
