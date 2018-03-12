d09904c50e8298bef331cb9c8022cf29c12e204d
TimVideos.us Robot <robot@timvideos.us>
2018-03-12 12:49:12 -0700

    Travis build #649 of dependabot/submodules/third_party/edid-decode-dcc8b8/v0.0.4-165-g0743c39 for PLATFORM=arty TARGET=base CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/0743c39095dcb69f09dd4cf36b23044e4b527d60

---
+++ archive/dependabot/submodules/third_party/edid-decode-dcc8b8/v0.0.4-165-g0743c39/arty/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "0743c39095dcb69f09dd4cf36b23044e4b527d60";
+const char* git_branch = "dependabot/submodules/third_party/edid-decode-dcc8b8";
+const char* git_describe = "v0.0.4-165-g0743c39";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
