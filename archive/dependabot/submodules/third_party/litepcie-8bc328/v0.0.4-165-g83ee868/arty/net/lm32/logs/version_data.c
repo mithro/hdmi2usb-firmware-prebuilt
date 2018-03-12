0c70d3b6460eedef8bad63862aa0a29c8dbee03b
TimVideos.us Robot <robot@timvideos.us>
2018-03-12 13:21:30 -0700

    Travis build #650 of dependabot/submodules/third_party/litepcie-8bc328/v0.0.4-165-g83ee868 for PLATFORM=arty TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/83ee86839c969feb81aafd42726ea528a75447f3

---
+++ archive/dependabot/submodules/third_party/litepcie-8bc328/v0.0.4-165-g83ee868/arty/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "83ee86839c969feb81aafd42726ea528a75447f3";
+const char* git_branch = "dependabot/submodules/third_party/litepcie-8bc328";
+const char* git_describe = "v0.0.4-165-g83ee868";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
