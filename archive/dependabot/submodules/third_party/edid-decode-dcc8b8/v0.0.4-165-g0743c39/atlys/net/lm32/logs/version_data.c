81db707d2824c3ef8b42806c375ed1a88f8ab9ed
TimVideos.us Robot <robot@timvideos.us>
2018-03-12 12:32:11 -0700

    Travis build #649 of dependabot/submodules/third_party/edid-decode-dcc8b8/v0.0.4-165-g0743c39 for PLATFORM=atlys TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/0743c39095dcb69f09dd4cf36b23044e4b527d60

---
+++ archive/dependabot/submodules/third_party/edid-decode-dcc8b8/v0.0.4-165-g0743c39/atlys/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "0743c39095dcb69f09dd4cf36b23044e4b527d60";
+const char* git_branch = "dependabot/submodules/third_party/edid-decode-dcc8b8";
+const char* git_describe = "v0.0.4-165-g0743c39";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
