63144d359d9e6dd7d59181976d744be565d182fa
TimVideos.us Robot <robot@timvideos.us>
2018-03-12 10:58:17 -0700

    Travis build #648 of new-litex/v0.0.4-169-g1ef75a5 for PLATFORM=mimasv2 TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/1ef75a538135ba4821207db374a2148c5dfe2b83

---
+++ archive/new-litex/v0.0.4-169-g1ef75a5/mimasv2/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MIMASV2
+#error "Version mismatch - PLATFORM_MIMASV2 not defined!"
+#endif
+const char* board = "mimasv2";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "1ef75a538135ba4821207db374a2148c5dfe2b83";
+const char* git_branch = "new-litex";
+const char* git_describe = "v0.0.4-169-g1ef75a5";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
