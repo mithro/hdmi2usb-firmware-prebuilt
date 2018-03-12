4a4fd1af95abc864f8bb2280eb54ad504176b0da
TimVideos.us Robot <robot@timvideos.us>
2018-03-12 14:14:46 -0700

    Travis build #649 of dependabot/submodules/third_party/edid-decode-dcc8b8/v0.0.4-165-g0743c39 for PLATFORM=opsis TARGET=video CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/0743c39095dcb69f09dd4cf36b23044e4b527d60

---
+++ archive/dependabot/submodules/third_party/edid-decode-dcc8b8/v0.0.4-165-g0743c39/opsis/video/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "0743c39095dcb69f09dd4cf36b23044e4b527d60";
+const char* git_branch = "dependabot/submodules/third_party/edid-decode-dcc8b8";
+const char* git_describe = "v0.0.4-165-g0743c39";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
