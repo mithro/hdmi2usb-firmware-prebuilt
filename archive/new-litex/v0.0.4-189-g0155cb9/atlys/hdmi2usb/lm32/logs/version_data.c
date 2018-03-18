f4635ee7d76833a2160490fb7a823393059a40dc
TimVideos.us Robot <robot@timvideos.us>
2018-03-18 02:01:23 -0700

    Travis build #675 of new-litex/v0.0.4-189-g0155cb9 for PLATFORM=atlys TARGET=hdmi2usb CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/0155cb9c1620e66aa88f82db38eb9d5eb5d1ba13

---
+++ archive/new-litex/v0.0.4-189-g0155cb9/atlys/hdmi2usb/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_HDMI2USB
+#error "Version mismatch - TARGET_HDMI2USB not defined!"
+#endif
+const char* target = "hdmi2usb";
+
+const char* git_commit = "0155cb9c1620e66aa88f82db38eb9d5eb5d1ba13";
+const char* git_branch = "new-litex";
+const char* git_describe = "v0.0.4-189-g0155cb9";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
