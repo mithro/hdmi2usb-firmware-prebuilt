ce45c73520adbd0ee6e9a1372d3fa22b7b7ed022
TimVideos.us Robot <robot@timvideos.us>
2018-03-18 14:28:55 -0700

    Travis build #676 of fix-generate/v0.0.4-191-gd70a2db for PLATFORM=pipistrello TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/d70a2db8233e195d884c152b7d729b707d48bacb

---
+++ archive/fix-generate/v0.0.4-191-gd70a2db/pipistrello/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_PIPISTRELLO
+#error "Version mismatch - PLATFORM_PIPISTRELLO not defined!"
+#endif
+const char* board = "pipistrello";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "d70a2db8233e195d884c152b7d729b707d48bacb";
+const char* git_branch = "fix-generate";
+const char* git_describe = "v0.0.4-191-gd70a2db";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
