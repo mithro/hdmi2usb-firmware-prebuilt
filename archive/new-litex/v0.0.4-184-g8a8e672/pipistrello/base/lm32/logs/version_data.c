04b3e858105d7fa7d4cbfc63aa5140a787e9b71a
TimVideos.us Robot <robot@timvideos.us>
2018-03-17 17:35:51 -0700

    Travis build #666 of new-litex/v0.0.4-184-g8a8e672 for PLATFORM=pipistrello TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/8a8e672e59c9a29a4073ad8b6e011db32b989506

---
+++ archive/new-litex/v0.0.4-184-g8a8e672/pipistrello/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_PIPISTRELLO
+#error "Version mismatch - PLATFORM_PIPISTRELLO not defined!"
+#endif
+const char* board = "pipistrello";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "8a8e672e59c9a29a4073ad8b6e011db32b989506";
+const char* git_branch = "new-litex";
+const char* git_describe = "v0.0.4-184-g8a8e672";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
