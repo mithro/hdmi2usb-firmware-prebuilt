6602662b1303d0a1343cbe403f1d5c99ae79ae6c
TimVideos.us Robot <robot@timvideos.us>
2018-03-18 15:37:18 -0700

    Travis build #676 of fix-generate/v0.0.4-191-gd70a2db for PLATFORM=opsis TARGET=video CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/d70a2db8233e195d884c152b7d729b707d48bacb

---
+++ archive/fix-generate/v0.0.4-191-gd70a2db/opsis/video/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "d70a2db8233e195d884c152b7d729b707d48bacb";
+const char* git_branch = "fix-generate";
+const char* git_describe = "v0.0.4-191-gd70a2db";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
