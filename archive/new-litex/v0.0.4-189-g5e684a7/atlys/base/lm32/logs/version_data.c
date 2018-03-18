aec00cd1f1228376ec6b096cd7d0a437216accc3
TimVideos.us Robot <robot@timvideos.us>
2018-03-17 21:39:21 -0700

    Travis build #673 of new-litex/v0.0.4-189-g5e684a7 for PLATFORM=atlys TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/5e684a7be4bc666a329cbdc59a2b360a672ede3e

---
+++ archive/new-litex/v0.0.4-189-g5e684a7/atlys/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "5e684a7be4bc666a329cbdc59a2b360a672ede3e";
+const char* git_branch = "new-litex";
+const char* git_describe = "v0.0.4-189-g5e684a7";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
