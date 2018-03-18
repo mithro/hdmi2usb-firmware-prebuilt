0b33962960f3084975cea93b46b8bc6b679926f5
TimVideos.us Robot <robot@timvideos.us>
2018-03-18 14:25:15 -0700

    Travis build #676 of fix-generate/v0.0.4-191-gd70a2db for PLATFORM=atlys TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/d70a2db8233e195d884c152b7d729b707d48bacb

---
+++ archive/fix-generate/v0.0.4-191-gd70a2db/atlys/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "d70a2db8233e195d884c152b7d729b707d48bacb";
+const char* git_branch = "fix-generate";
+const char* git_describe = "v0.0.4-191-gd70a2db";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
