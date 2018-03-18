b9c7711a94b5abad900a82fd9350cb33276229b0
TimVideos.us Robot <robot@timvideos.us>
2018-03-17 17:43:18 -0700

    Travis build #666 of new-litex/v0.0.4-184-g8a8e672 for PLATFORM=opsis TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/8a8e672e59c9a29a4073ad8b6e011db32b989506

---
+++ archive/new-litex/v0.0.4-184-g8a8e672/opsis/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "8a8e672e59c9a29a4073ad8b6e011db32b989506";
+const char* git_branch = "new-litex";
+const char* git_describe = "v0.0.4-184-g8a8e672";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
