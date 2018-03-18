864da8e579edd1f7a37f6103e7531b0c385e6ec2
TimVideos.us Robot <robot@timvideos.us>
2018-03-17 22:54:54 -0700

    Travis build #675 of new-litex/v0.0.4-189-g0155cb9 for PLATFORM=arty TARGET=net CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/0155cb9c1620e66aa88f82db38eb9d5eb5d1ba13

---
+++ archive/new-litex/v0.0.4-189-g0155cb9/arty/net/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "0155cb9c1620e66aa88f82db38eb9d5eb5d1ba13";
+const char* git_branch = "new-litex";
+const char* git_describe = "v0.0.4-189-g0155cb9";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
