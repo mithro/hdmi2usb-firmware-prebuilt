8f44e4f2f2d70875bdd86a1c44f8fcbb94cd694e
TimVideos.us Robot <robot@timvideos.us>
2018-03-17 22:33:57 -0700

    Travis build #675 of new-litex/v0.0.4-189-g0155cb9 for PLATFORM=netv2 TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/0155cb9c1620e66aa88f82db38eb9d5eb5d1ba13

---
+++ archive/new-litex/v0.0.4-189-g0155cb9/netv2/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NETV2
+#error "Version mismatch - PLATFORM_NETV2 not defined!"
+#endif
+const char* board = "netv2";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "0155cb9c1620e66aa88f82db38eb9d5eb5d1ba13";
+const char* git_branch = "new-litex";
+const char* git_describe = "v0.0.4-189-g0155cb9";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
