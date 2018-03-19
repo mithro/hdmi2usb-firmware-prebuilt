6aced22882c3a7be0b665568c107c0c1b40b5660
TimVideos.us Robot <robot@timvideos.us>
2018-03-18 18:42:09 -0700

    Travis build #678 of nullglob-fix/v0.0.4-193-g2f752af for PLATFORM=atlys TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/2f752afa2dcff8f338fccc6395ed7b3f65d1dfe9

---
+++ archive/nullglob-fix/v0.0.4-193-g2f752af/atlys/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "2f752afa2dcff8f338fccc6395ed7b3f65d1dfe9";
+const char* git_branch = "nullglob-fix";
+const char* git_describe = "v0.0.4-193-g2f752af";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
