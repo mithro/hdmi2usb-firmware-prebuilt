c48a0d96e7859e4c1d28df83a9537708a1e083b3
TimVideos.us Robot <robot@timvideos.us>
2018-03-18 18:45:11 -0700

    Travis build #678 of nullglob-fix/v0.0.4-193-g2f752af for PLATFORM=arty TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/2f752afa2dcff8f338fccc6395ed7b3f65d1dfe9

---
+++ archive/nullglob-fix/v0.0.4-193-g2f752af/arty/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "2f752afa2dcff8f338fccc6395ed7b3f65d1dfe9";
+const char* git_branch = "nullglob-fix";
+const char* git_describe = "v0.0.4-193-g2f752af";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
