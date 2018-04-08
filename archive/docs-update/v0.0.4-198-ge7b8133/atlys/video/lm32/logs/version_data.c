b4ec68d2d0ddab2e92802db2d33f9ef3d6a13b2b
TimVideos.us Robot <robot@timvideos.us>
2018-04-08 13:39:58 -0700

    Travis build #683 of docs-update/v0.0.4-198-ge7b8133 for PLATFORM=atlys TARGET=video CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/e7b8133dc6ef08f1d95d26d03ce6aee6821daeff

---
+++ archive/docs-update/v0.0.4-198-ge7b8133/atlys/video/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "e7b8133dc6ef08f1d95d26d03ce6aee6821daeff";
+const char* git_branch = "docs-update";
+const char* git_describe = "v0.0.4-198-ge7b8133";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
