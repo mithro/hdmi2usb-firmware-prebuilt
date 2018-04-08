8a2550cee074ceffdb742f5d556431724e473f56
TimVideos.us Robot <robot@timvideos.us>
2018-04-07 18:02:18 -0700

    Travis build #679 of flash-firmware-fix/v0.0.4-195-gec921a5 for PLATFORM=arty TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/ec921a537003c53a665a2a7b65ee81f6aed53513

---
+++ archive/flash-firmware-fix/v0.0.4-195-gec921a5/arty/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "ec921a537003c53a665a2a7b65ee81f6aed53513";
+const char* git_branch = "flash-firmware-fix";
+const char* git_describe = "v0.0.4-195-gec921a5";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
