0d7882219e404e6ea28c01280b21266ac01645e2
TimVideos.us Robot <robot@timvideos.us>
2018-04-08 12:36:48 -0700

    Travis build #683 of docs-update/v0.0.4-198-ge7b8133 for PLATFORM=pipistrello TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/e7b8133dc6ef08f1d95d26d03ce6aee6821daeff

---
+++ archive/docs-update/v0.0.4-198-ge7b8133/pipistrello/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_PIPISTRELLO
+#error "Version mismatch - PLATFORM_PIPISTRELLO not defined!"
+#endif
+const char* board = "pipistrello";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "e7b8133dc6ef08f1d95d26d03ce6aee6821daeff";
+const char* git_branch = "docs-update";
+const char* git_describe = "v0.0.4-198-ge7b8133";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
