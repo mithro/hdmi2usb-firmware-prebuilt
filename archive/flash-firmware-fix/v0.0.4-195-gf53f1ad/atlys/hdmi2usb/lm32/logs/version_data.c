3c9f7247b09d82d5dd717010a805ed12f1706df1
TimVideos.us Robot <robot@timvideos.us>
2018-04-07 19:59:10 -0700

    Travis build #680 of flash-firmware-fix/v0.0.4-195-gf53f1ad for PLATFORM=atlys TARGET=hdmi2usb CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/f53f1ad393d2749e12260b7c666c572d62738c7b

---
+++ archive/flash-firmware-fix/v0.0.4-195-gf53f1ad/atlys/hdmi2usb/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_HDMI2USB
+#error "Version mismatch - TARGET_HDMI2USB not defined!"
+#endif
+const char* target = "hdmi2usb";
+
+const char* git_commit = "f53f1ad393d2749e12260b7c666c572d62738c7b";
+const char* git_branch = "flash-firmware-fix";
+const char* git_describe = "v0.0.4-195-gf53f1ad";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
