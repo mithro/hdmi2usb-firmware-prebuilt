081711105c2810a1ea7c42b8f9e86c1bcc8047bd
TimVideos.us Robot <robot@timvideos.us>
2018-04-07 19:11:31 -0700

    Travis build #680 of flash-firmware-fix/v0.0.4-195-gf53f1ad for PLATFORM=arty TARGET=net CPU=or1k.linux FIRMWARE=linux
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/f53f1ad393d2749e12260b7c666c572d62738c7b

---
+++ archive/flash-firmware-fix/v0.0.4-195-gf53f1ad/arty/net/or1k.linux/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "f53f1ad393d2749e12260b7c666c572d62738c7b";
+const char* git_branch = "flash-firmware-fix";
+const char* git_describe = "v0.0.4-195-gf53f1ad";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
