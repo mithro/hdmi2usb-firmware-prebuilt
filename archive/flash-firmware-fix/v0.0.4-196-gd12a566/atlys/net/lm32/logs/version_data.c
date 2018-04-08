f100e8f8c5c6bfd16d77fd3d6312930b85b07811
TimVideos.us Robot <robot@timvideos.us>
2018-04-08 10:40:30 -0700

    Travis build #681 of flash-firmware-fix/v0.0.4-196-gd12a566 for PLATFORM=atlys TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/d12a56665870234d17e0d980293431fd33749b40

---
+++ archive/flash-firmware-fix/v0.0.4-196-gd12a566/atlys/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "d12a56665870234d17e0d980293431fd33749b40";
+const char* git_branch = "flash-firmware-fix";
+const char* git_describe = "v0.0.4-196-gd12a566";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
