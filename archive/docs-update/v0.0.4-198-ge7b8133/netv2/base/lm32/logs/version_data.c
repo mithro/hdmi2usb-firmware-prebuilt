557d2179283fe69b8338923b709773be62e2700c
TimVideos.us Robot <robot@timvideos.us>
2018-04-08 12:11:02 -0700

    Travis build #683 of docs-update/v0.0.4-198-ge7b8133 for PLATFORM=netv2 TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/e7b8133dc6ef08f1d95d26d03ce6aee6821daeff

---
+++ archive/docs-update/v0.0.4-198-ge7b8133/netv2/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NETV2
+#error "Version mismatch - PLATFORM_NETV2 not defined!"
+#endif
+const char* board = "netv2";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "e7b8133dc6ef08f1d95d26d03ce6aee6821daeff";
+const char* git_branch = "docs-update";
+const char* git_describe = "v0.0.4-198-ge7b8133";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
