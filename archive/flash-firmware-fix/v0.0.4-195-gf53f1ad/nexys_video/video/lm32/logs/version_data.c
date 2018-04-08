bb09fa5c267e7021d57c26444740f3db2340e9a3
TimVideos.us Robot <robot@timvideos.us>
2018-04-07 19:29:19 -0700

    Travis build #680 of flash-firmware-fix/v0.0.4-195-gf53f1ad for PLATFORM=nexys_video TARGET=video CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/f53f1ad393d2749e12260b7c666c572d62738c7b

---
+++ archive/flash-firmware-fix/v0.0.4-195-gf53f1ad/nexys_video/video/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NEXYS_VIDEO
+#error "Version mismatch - PLATFORM_NEXYS_VIDEO not defined!"
+#endif
+const char* board = "nexys_video";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "f53f1ad393d2749e12260b7c666c572d62738c7b";
+const char* git_branch = "flash-firmware-fix";
+const char* git_describe = "v0.0.4-195-gf53f1ad";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
