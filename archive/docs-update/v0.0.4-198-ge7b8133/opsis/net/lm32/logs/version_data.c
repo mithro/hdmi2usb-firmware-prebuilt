6a3440e4618bedadbe6f12987500f12ea58a38b9
TimVideos.us Robot <robot@timvideos.us>
2018-04-08 12:33:50 -0700

    Travis build #683 of docs-update/v0.0.4-198-ge7b8133 for PLATFORM=opsis TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/e7b8133dc6ef08f1d95d26d03ce6aee6821daeff

---
+++ archive/docs-update/v0.0.4-198-ge7b8133/opsis/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "e7b8133dc6ef08f1d95d26d03ce6aee6821daeff";
+const char* git_branch = "docs-update";
+const char* git_describe = "v0.0.4-198-ge7b8133";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
