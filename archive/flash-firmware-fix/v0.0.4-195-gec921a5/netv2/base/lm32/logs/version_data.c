4cb47e90ecc4f34d59b66847a05bdc87300e8193
TimVideos.us Robot <robot@timvideos.us>
2018-04-07 17:50:33 -0700

    Travis build #679 of flash-firmware-fix/v0.0.4-195-gec921a5 for PLATFORM=netv2 TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/ec921a537003c53a665a2a7b65ee81f6aed53513

---
+++ archive/flash-firmware-fix/v0.0.4-195-gec921a5/netv2/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NETV2
+#error "Version mismatch - PLATFORM_NETV2 not defined!"
+#endif
+const char* board = "netv2";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "ec921a537003c53a665a2a7b65ee81f6aed53513";
+const char* git_branch = "flash-firmware-fix";
+const char* git_describe = "v0.0.4-195-gec921a5";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
