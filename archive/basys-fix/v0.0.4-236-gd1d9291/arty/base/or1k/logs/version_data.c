bc2678b1d0eca7438465f660c6c2789d90222509
TimVideos.us Robot <robot@timvideos.us>
2018-05-18 16:55:30 -0700

    Travis build #686 of basys-fix/v0.0.4-236-gd1d9291 for PLATFORM=arty TARGET=net CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/d1d9291abc4523545b62ba5dd5ba5df7a89cf33f

---
+++ archive/basys-fix/v0.0.4-236-gd1d9291/arty/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "d1d9291abc4523545b62ba5dd5ba5df7a89cf33f";
+const char* git_branch = "basys-fix";
+const char* git_describe = "v0.0.4-236-gd1d9291";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
---
+++ archive/basys-fix/v0.0.4-236-gd1d9291/arty/net/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "d1d9291abc4523545b62ba5dd5ba5df7a89cf33f";
+const char* git_branch = "basys-fix";
+const char* git_describe = "v0.0.4-236-gd1d9291";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
