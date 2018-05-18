e3a0f9695b1a008f20fa73ff513c6a454aba0e28
TimVideos.us Robot <robot@timvideos.us>
2018-05-18 16:28:35 -0700

    Travis build #686 of basys-fix/v0.0.4-236-gd1d9291 for PLATFORM=galatea TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/d1d9291abc4523545b62ba5dd5ba5df7a89cf33f

---
+++ archive/basys-fix/v0.0.4-236-gd1d9291/galatea/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_GALATEA
+#error "Version mismatch - PLATFORM_GALATEA not defined!"
+#endif
+const char* board = "galatea";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "d1d9291abc4523545b62ba5dd5ba5df7a89cf33f";
+const char* git_branch = "basys-fix";
+const char* git_describe = "v0.0.4-236-gd1d9291";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
