10db3f30aef5d0b7e22568ca77dd123f31be8787
TimVideos.us Robot <robot@timvideos.us>
2018-05-18 15:46:58 -0700

    Travis build #685 of deps-roll/v0.0.4-236-gb828a49 for PLATFORM=atlys TARGET=base CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/b828a49d67443cbec9ce9aee0a0e0b9dc60553e1

---
+++ archive/deps-roll/v0.0.4-236-gb828a49/atlys/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "b828a49d67443cbec9ce9aee0a0e0b9dc60553e1";
+const char* git_branch = "deps-roll";
+const char* git_describe = "v0.0.4-236-gb828a49";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
