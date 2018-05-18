91187d1996c9ea9729ba29df2b3c1d387e118c27
TimVideos.us Robot <robot@timvideos.us>
2018-05-18 15:15:16 -0700

    Travis build #685 of deps-roll/v0.0.4-236-gb828a49 for PLATFORM=netv2 TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/b828a49d67443cbec9ce9aee0a0e0b9dc60553e1

---
+++ archive/deps-roll/v0.0.4-236-gb828a49/netv2/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NETV2
+#error "Version mismatch - PLATFORM_NETV2 not defined!"
+#endif
+const char* board = "netv2";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "b828a49d67443cbec9ce9aee0a0e0b9dc60553e1";
+const char* git_branch = "deps-roll";
+const char* git_describe = "v0.0.4-236-gb828a49";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
