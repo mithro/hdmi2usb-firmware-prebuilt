d1f9363986b3c140fdf89b47b9e923c9738cca01
TimVideos.us Robot <robot@timvideos.us>
2018-05-19 15:11:00 -0700

    Travis build #689 of docs-update/v0.0.4-240-gc8349e4 for PLATFORM=arty TARGET=base CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/c8349e4bb33c1d7625a6bb1ccea0c3b285c14481

---
+++ archive/docs-update/v0.0.4-240-gc8349e4/arty/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "c8349e4bb33c1d7625a6bb1ccea0c3b285c14481";
+const char* git_branch = "docs-update";
+const char* git_describe = "v0.0.4-240-gc8349e4";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
