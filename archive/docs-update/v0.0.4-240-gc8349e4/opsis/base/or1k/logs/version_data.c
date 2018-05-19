49d8c9fdeee87de96ffd7d6f1136c4c308873eb0
TimVideos.us Robot <robot@timvideos.us>
2018-05-19 15:14:45 -0700

    Travis build #689 of docs-update/v0.0.4-240-gc8349e4 for PLATFORM=opsis TARGET=base CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/c8349e4bb33c1d7625a6bb1ccea0c3b285c14481

---
+++ archive/docs-update/v0.0.4-240-gc8349e4/opsis/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "c8349e4bb33c1d7625a6bb1ccea0c3b285c14481";
+const char* git_branch = "docs-update";
+const char* git_describe = "v0.0.4-240-gc8349e4";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
