e95f369ad04b7bcfe64f246b8364f7096c81da61
TimVideos.us Robot <robot@timvideos.us>
2018-05-19 12:35:33 -0700

    Travis build #687 of autolabeller-fix/v0.0.4-240-gdf73824 for PLATFORM=waxwing TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/df738247ec3fcc3f5263f5e74e01d707a6712b4c

---
+++ archive/autolabeller-fix/v0.0.4-240-gdf73824/waxwing/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_WAXWING
+#error "Version mismatch - PLATFORM_WAXWING not defined!"
+#endif
+const char* board = "waxwing";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "df738247ec3fcc3f5263f5e74e01d707a6712b4c";
+const char* git_branch = "autolabeller-fix";
+const char* git_describe = "v0.0.4-240-gdf73824";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
