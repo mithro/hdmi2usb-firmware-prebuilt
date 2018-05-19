fb504988c9ecdf7fb4d2d092f76792b46ea898c3
TimVideos.us Robot <robot@timvideos.us>
2018-05-19 15:22:37 -0700

    Travis build #689 of docs-update/v0.0.4-240-gc8349e4 for PLATFORM=atlys TARGET=net CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/c8349e4bb33c1d7625a6bb1ccea0c3b285c14481

---
+++ archive/docs-update/v0.0.4-240-gc8349e4/atlys/net/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "c8349e4bb33c1d7625a6bb1ccea0c3b285c14481";
+const char* git_branch = "docs-update";
+const char* git_describe = "v0.0.4-240-gc8349e4";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
