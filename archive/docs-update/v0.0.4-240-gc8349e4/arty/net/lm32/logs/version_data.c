ea5c70279af02afeaba10f54e8f280684394c929
TimVideos.us Robot <robot@timvideos.us>
2018-05-19 14:38:44 -0700

    Travis build #689 of docs-update/v0.0.4-240-gc8349e4 for PLATFORM=arty TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/c8349e4bb33c1d7625a6bb1ccea0c3b285c14481

---
+++ archive/docs-update/v0.0.4-240-gc8349e4/arty/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "c8349e4bb33c1d7625a6bb1ccea0c3b285c14481";
+const char* git_branch = "docs-update";
+const char* git_describe = "v0.0.4-240-gc8349e4";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
