1bda0220548a7eff00a3de9bcff1407a832c601f
TimVideos.us Robot <robot@timvideos.us>
2018-05-19 12:28:50 -0700

    Travis build #687 of autolabeller-fix/v0.0.4-240-gdf73824 for PLATFORM=nexys_video TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/df738247ec3fcc3f5263f5e74e01d707a6712b4c

---
+++ archive/autolabeller-fix/v0.0.4-240-gdf73824/nexys_video/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NEXYS_VIDEO
+#error "Version mismatch - PLATFORM_NEXYS_VIDEO not defined!"
+#endif
+const char* board = "nexys_video";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "df738247ec3fcc3f5263f5e74e01d707a6712b4c";
+const char* git_branch = "autolabeller-fix";
+const char* git_describe = "v0.0.4-240-gdf73824";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
---
+++ archive/autolabeller-fix/v0.0.4-240-gdf73824/nexys_video/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NEXYS_VIDEO
+#error "Version mismatch - PLATFORM_NEXYS_VIDEO not defined!"
+#endif
+const char* board = "nexys_video";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "df738247ec3fcc3f5263f5e74e01d707a6712b4c";
+const char* git_branch = "autolabeller-fix";
+const char* git_describe = "v0.0.4-240-gdf73824";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
