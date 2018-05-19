f7d6d040e627b2380d4e8d52a0c3f738d2072b8b
TimVideos.us Robot <robot@timvideos.us>
2018-05-19 12:23:39 -0700

    Travis build #687 of autolabeller-fix/v0.0.4-240-gdf73824 for PLATFORM=opsis TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/df738247ec3fcc3f5263f5e74e01d707a6712b4c

---
+++ archive/autolabeller-fix/v0.0.4-240-gdf73824/opsis/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "df738247ec3fcc3f5263f5e74e01d707a6712b4c";
+const char* git_branch = "autolabeller-fix";
+const char* git_describe = "v0.0.4-240-gdf73824";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
