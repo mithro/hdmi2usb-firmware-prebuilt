6bbb84e29a1582f576daf9f0c43d89316b6e6451
TimVideos.us Robot <robot@timvideos.us>
2018-05-19 12:53:00 -0700

    Travis build #687 of autolabeller-fix/v0.0.4-240-gdf73824 for PLATFORM=arty TARGET=net CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/df738247ec3fcc3f5263f5e74e01d707a6712b4c

---
+++ archive/autolabeller-fix/v0.0.4-240-gdf73824/arty/net/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "df738247ec3fcc3f5263f5e74e01d707a6712b4c";
+const char* git_branch = "autolabeller-fix";
+const char* git_describe = "v0.0.4-240-gdf73824";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
