03b902140e3b15af04d474fb85de6541d1c1ca0c
TimVideos.us Robot <robot@timvideos.us>
2018-05-20 17:49:55 -0700

    Travis build #690 of travis-update/v0.0.4-240-g5fdefa6 for PLATFORM=pipistrello TARGET=base CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/5fdefa6b7e7a23ec2183b482ab6f02473ef5e490

---
+++ archive/travis-update/v0.0.4-240-g5fdefa6/pipistrello/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_PIPISTRELLO
+#error "Version mismatch - PLATFORM_PIPISTRELLO not defined!"
+#endif
+const char* board = "pipistrello";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "5fdefa6b7e7a23ec2183b482ab6f02473ef5e490";
+const char* git_branch = "travis-update";
+const char* git_describe = "v0.0.4-240-g5fdefa6";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
