e4470ce7f62cb26d3de5aafda3b1193a9098efcd
TimVideos.us Robot <robot@timvideos.us>
2018-05-20 17:27:58 -0700

    Travis build #690 of travis-update/v0.0.4-240-g5fdefa6 for PLATFORM=nexys_video TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/5fdefa6b7e7a23ec2183b482ab6f02473ef5e490

---
+++ archive/travis-update/v0.0.4-240-g5fdefa6/nexys_video/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NEXYS_VIDEO
+#error "Version mismatch - PLATFORM_NEXYS_VIDEO not defined!"
+#endif
+const char* board = "nexys_video";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "5fdefa6b7e7a23ec2183b482ab6f02473ef5e490";
+const char* git_branch = "travis-update";
+const char* git_describe = "v0.0.4-240-g5fdefa6";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
