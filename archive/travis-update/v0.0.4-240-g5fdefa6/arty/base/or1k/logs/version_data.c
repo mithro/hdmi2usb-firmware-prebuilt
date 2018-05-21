16e6c903487ee889556e47b30d307666df83ca4b
TimVideos.us Robot <robot@timvideos.us>
2018-05-20 17:48:05 -0700

    Travis build #690 of travis-update/v0.0.4-240-g5fdefa6 for PLATFORM=arty TARGET=base CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/5fdefa6b7e7a23ec2183b482ab6f02473ef5e490

---
+++ archive/travis-update/v0.0.4-240-g5fdefa6/arty/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "5fdefa6b7e7a23ec2183b482ab6f02473ef5e490";
+const char* git_branch = "travis-update";
+const char* git_describe = "v0.0.4-240-g5fdefa6";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
