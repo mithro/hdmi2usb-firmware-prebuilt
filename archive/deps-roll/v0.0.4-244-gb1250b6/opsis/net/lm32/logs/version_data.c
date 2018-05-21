0713d81deb8e8c78f7862e6c878db9e41d89e1e2
TimVideos.us Robot <robot@timvideos.us>
2018-05-21 11:52:04 -0700

    Travis build #691 of deps-roll/v0.0.4-244-gb1250b6 for PLATFORM=opsis TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/b1250b65ecb447ebef054e6b2a599139f9061951

---
+++ archive/deps-roll/v0.0.4-244-gb1250b6/opsis/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "b1250b65ecb447ebef054e6b2a599139f9061951";
+const char* git_branch = "deps-roll";
+const char* git_describe = "v0.0.4-244-gb1250b6";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
