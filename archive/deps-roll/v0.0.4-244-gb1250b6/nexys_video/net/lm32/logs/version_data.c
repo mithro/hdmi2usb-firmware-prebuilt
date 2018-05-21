ac1a9807e973324b14dfed82238f7296f47ed4ed
TimVideos.us Robot <robot@timvideos.us>
2018-05-21 11:59:01 -0700

    Travis build #691 of deps-roll/v0.0.4-244-gb1250b6 for PLATFORM=nexys_video TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/b1250b65ecb447ebef054e6b2a599139f9061951

---
+++ archive/deps-roll/v0.0.4-244-gb1250b6/nexys_video/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NEXYS_VIDEO
+#error "Version mismatch - PLATFORM_NEXYS_VIDEO not defined!"
+#endif
+const char* board = "nexys_video";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "b1250b65ecb447ebef054e6b2a599139f9061951";
+const char* git_branch = "deps-roll";
+const char* git_describe = "v0.0.4-244-gb1250b6";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
