05b882364d9bf4331ca986318d014ad48adf3fcb
TimVideos.us Robot <robot@timvideos.us>
2018-05-20 17:15:48 -0700

    Travis build #690 of travis-update/v0.0.4-240-g5fdefa6 for PLATFORM=atlys TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/5fdefa6b7e7a23ec2183b482ab6f02473ef5e490

---
+++ archive/travis-update/v0.0.4-240-g5fdefa6/atlys/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "5fdefa6b7e7a23ec2183b482ab6f02473ef5e490";
+const char* git_branch = "travis-update";
+const char* git_describe = "v0.0.4-240-g5fdefa6";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
