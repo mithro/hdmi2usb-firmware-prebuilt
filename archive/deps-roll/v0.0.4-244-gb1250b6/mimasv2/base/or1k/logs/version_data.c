4a7fd06d09a1ead625c7e77f6dc2e427272bebc3
TimVideos.us Robot <robot@timvideos.us>
2018-05-21 12:34:43 -0700

    Travis build #691 of deps-roll/v0.0.4-244-gb1250b6 for PLATFORM=mimasv2 TARGET=base CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/b1250b65ecb447ebef054e6b2a599139f9061951

---
+++ archive/deps-roll/v0.0.4-244-gb1250b6/mimasv2/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MIMASV2
+#error "Version mismatch - PLATFORM_MIMASV2 not defined!"
+#endif
+const char* board = "mimasv2";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "b1250b65ecb447ebef054e6b2a599139f9061951";
+const char* git_branch = "deps-roll";
+const char* git_describe = "v0.0.4-244-gb1250b6";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
