d2e32de4a1a0f810a77da9e963af8f2b2a6e1db8
TimVideos.us Robot <robot@timvideos.us>
2018-08-22 21:57:04 -0700

    Travis build #693 of tinyfpga-support/v0.0.4-253-gd36da93 for PLATFORM=minispartan6 TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/d36da932ce53c4f93c1429ee55f15d080bff3c4e

---
+++ archive/tinyfpga-support/v0.0.4-253-gd36da93/minispartan6/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MINISPARTAN6
+#error "Version mismatch - PLATFORM_MINISPARTAN6 not defined!"
+#endif
+const char* board = "minispartan6";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "d36da932ce53c4f93c1429ee55f15d080bff3c4e";
+const char* git_branch = "tinyfpga-support";
+const char* git_describe = "v0.0.4-253-gd36da93";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
