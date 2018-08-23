731cccce317cb5bda670f5c6cb1f3c35cdc4adc8
TimVideos.us Robot <robot@timvideos.us>
2018-08-22 21:44:39 -0700

    Travis build #692 of tinyfpga-support/v0.0.4-253-g807b07b for PLATFORM=arty TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/807b07b504837d8e446ed7aceeb9878ee0942c65

---
+++ archive/tinyfpga-support/v0.0.4-253-g807b07b/arty/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "807b07b504837d8e446ed7aceeb9878ee0942c65";
+const char* git_branch = "tinyfpga-support";
+const char* git_describe = "v0.0.4-253-g807b07b";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
