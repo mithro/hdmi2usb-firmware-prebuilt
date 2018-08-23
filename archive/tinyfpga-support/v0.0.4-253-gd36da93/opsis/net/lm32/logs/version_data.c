8ddb3ba7011a6b3555bf80f56293ef2fe7a7ab12
TimVideos.us Robot <robot@timvideos.us>
2018-08-22 22:17:01 -0700

    Travis build #693 of tinyfpga-support/v0.0.4-253-gd36da93 for PLATFORM=opsis TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/d36da932ce53c4f93c1429ee55f15d080bff3c4e

---
+++ archive/tinyfpga-support/v0.0.4-253-gd36da93/opsis/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "d36da932ce53c4f93c1429ee55f15d080bff3c4e";
+const char* git_branch = "tinyfpga-support";
+const char* git_describe = "v0.0.4-253-gd36da93";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
