a13c879764fd5e17e25a3bb9968cbe01650147cb
TimVideos.us Robot <robot@timvideos.us>
2018-08-23 00:25:28 -0700

    Travis build #697 of tinyfpga-support/v0.0.4-255-gfdbd5ed for PLATFORM=galatea TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/fdbd5ed891bfbef0989b5a8261c07508a46ec680

---
+++ archive/tinyfpga-support/v0.0.4-255-gfdbd5ed/galatea/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_GALATEA
+#error "Version mismatch - PLATFORM_GALATEA not defined!"
+#endif
+const char* board = "galatea";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "fdbd5ed891bfbef0989b5a8261c07508a46ec680";
+const char* git_branch = "tinyfpga-support";
+const char* git_describe = "v0.0.4-255-gfdbd5ed";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
