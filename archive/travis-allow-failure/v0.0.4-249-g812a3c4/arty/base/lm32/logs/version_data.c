cee4adf33b208c3fa8ab26d10d8270e7188110c8
TimVideos.us Robot <robot@timvideos.us>
2018-08-26 21:39:34 -0700

    Travis build #699 of travis-allow-failure/v0.0.4-249-g812a3c4 for PLATFORM=arty TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/812a3c4783da93b46376aecd18e6dac7df7d876d

---
+++ archive/travis-allow-failure/v0.0.4-249-g812a3c4/arty/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "812a3c4783da93b46376aecd18e6dac7df7d876d";
+const char* git_branch = "travis-allow-failure";
+const char* git_describe = "v0.0.4-249-g812a3c4";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
