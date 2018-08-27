1742fb9cb2fe506f84e2041f82cdb96b188f6adc
TimVideos.us Robot <robot@timvideos.us>
2018-08-26 21:48:24 -0700

    Travis build #699 of travis-allow-failure/v0.0.4-249-g812a3c4 for PLATFORM=opsis TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/812a3c4783da93b46376aecd18e6dac7df7d876d

---
+++ archive/travis-allow-failure/v0.0.4-249-g812a3c4/opsis/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "812a3c4783da93b46376aecd18e6dac7df7d876d";
+const char* git_branch = "travis-allow-failure";
+const char* git_describe = "v0.0.4-249-g812a3c4";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
