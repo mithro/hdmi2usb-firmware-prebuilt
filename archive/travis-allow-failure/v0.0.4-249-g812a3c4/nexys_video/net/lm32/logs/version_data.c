e8105235663838086a07441a04d85778b20c1bef
TimVideos.us Robot <robot@timvideos.us>
2018-08-26 22:00:40 -0700

    Travis build #699 of travis-allow-failure/v0.0.4-249-g812a3c4 for PLATFORM=nexys_video TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/812a3c4783da93b46376aecd18e6dac7df7d876d

---
+++ archive/travis-allow-failure/v0.0.4-249-g812a3c4/nexys_video/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NEXYS_VIDEO
+#error "Version mismatch - PLATFORM_NEXYS_VIDEO not defined!"
+#endif
+const char* board = "nexys_video";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "812a3c4783da93b46376aecd18e6dac7df7d876d";
+const char* git_branch = "travis-allow-failure";
+const char* git_describe = "v0.0.4-249-g812a3c4";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
