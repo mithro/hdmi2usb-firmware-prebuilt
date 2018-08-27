29398a6686c9974bbba8375631a58d03c90463fb
TimVideos.us Robot <robot@timvideos.us>
2018-08-26 21:45:28 -0700

    Travis build #699 of travis-allow-failure/v0.0.4-249-g812a3c4 for PLATFORM=atlys TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/812a3c4783da93b46376aecd18e6dac7df7d876d

---
+++ archive/travis-allow-failure/v0.0.4-249-g812a3c4/atlys/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "812a3c4783da93b46376aecd18e6dac7df7d876d";
+const char* git_branch = "travis-allow-failure";
+const char* git_describe = "v0.0.4-249-g812a3c4";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
