ac5233e74780a7cffcd9666863945a3df3523323
TimVideos.us Robot <robot@timvideos.us>
2018-08-27 18:54:26 -0700

    Travis build #701 of small-fixes/v0.0.4-262-gb593258 for PLATFORM=mimasv2 TARGET=base CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/b593258135164df19848f42e542c8a4d8fd5a45e

---
+++ archive/small-fixes/v0.0.4-262-gb593258/mimasv2/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MIMASV2
+#error "Version mismatch - PLATFORM_MIMASV2 not defined!"
+#endif
+const char* board = "mimasv2";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "b593258135164df19848f42e542c8a4d8fd5a45e";
+const char* git_branch = "small-fixes";
+const char* git_describe = "v0.0.4-262-gb593258";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
