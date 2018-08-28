0ab7f8b2f731542c245d0373c1dcf4886da4de9c
TimVideos.us Robot <robot@timvideos.us>
2018-08-27 18:15:17 -0700

    Travis build #701 of small-fixes/v0.0.4-262-gb593258 for PLATFORM=opsis TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/b593258135164df19848f42e542c8a4d8fd5a45e

---
+++ archive/small-fixes/v0.0.4-262-gb593258/opsis/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "b593258135164df19848f42e542c8a4d8fd5a45e";
+const char* git_branch = "small-fixes";
+const char* git_describe = "v0.0.4-262-gb593258";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
