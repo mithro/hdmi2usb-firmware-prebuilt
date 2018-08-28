24d4461c36e03a7ded3416a07884f72ae691230c
TimVideos.us Robot <robot@timvideos.us>
2018-08-27 18:06:52 -0700

    Travis build #701 of small-fixes/v0.0.4-262-gb593258 for PLATFORM=atlys TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/b593258135164df19848f42e542c8a4d8fd5a45e

---
+++ archive/small-fixes/v0.0.4-262-gb593258/atlys/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "b593258135164df19848f42e542c8a4d8fd5a45e";
+const char* git_branch = "small-fixes";
+const char* git_describe = "v0.0.4-262-gb593258";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
