c5ccbb8a83f8b5fc2ec89fda91d99235831c9454
TimVideos.us Robot <robot@timvideos.us>
2018-08-31 11:44:15 -0700

    Travis build #704 of travis-update/v0.0.4-268-g1177134 for PLATFORM=mimasv2 TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/1177134e358dd56854a6d17bed4b5ec8c3df2864

---
+++ archive/travis-update/v0.0.4-268-g1177134/mimasv2/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MIMASV2
+#error "Version mismatch - PLATFORM_MIMASV2 not defined!"
+#endif
+const char* board = "mimasv2";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "1177134e358dd56854a6d17bed4b5ec8c3df2864";
+const char* git_branch = "travis-update";
+const char* git_describe = "v0.0.4-268-g1177134";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
