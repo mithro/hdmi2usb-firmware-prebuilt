9afcf01dfd0c0dd28c1f2be4c7714f07e082cdac
TimVideos.us Robot <robot@timvideos.us>
2018-09-01 13:18:42 -0700

    Travis build #709 of or1k-rom-size-fix/v0.0.4-274-g41bfac0 for PLATFORM=nexys_video TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/41bfac0acb637cb37bae8deab651006cf0031af0

---
+++ archive/or1k-rom-size-fix/v0.0.4-274-g41bfac0/nexys_video/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NEXYS_VIDEO
+#error "Version mismatch - PLATFORM_NEXYS_VIDEO not defined!"
+#endif
+const char* board = "nexys_video";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "41bfac0acb637cb37bae8deab651006cf0031af0";
+const char* git_branch = "or1k-rom-size-fix";
+const char* git_describe = "v0.0.4-274-g41bfac0";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
