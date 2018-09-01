3435dc6414a07e9483ec1849396985b22f94b459
TimVideos.us Robot <robot@timvideos.us>
2018-09-01 15:47:16 -0700

    Travis build #712 of or1k-rom-size-fix/v0.0.4-276-g754f0a3 for PLATFORM=minispartan6 TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/754f0a39caaa5e2d4b8232ce164e4d5285191c49

---
+++ archive/or1k-rom-size-fix/v0.0.4-276-g754f0a3/minispartan6/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MINISPARTAN6
+#error "Version mismatch - PLATFORM_MINISPARTAN6 not defined!"
+#endif
+const char* board = "minispartan6";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "754f0a39caaa5e2d4b8232ce164e4d5285191c49";
+const char* git_branch = "or1k-rom-size-fix";
+const char* git_describe = "v0.0.4-276-g754f0a3";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
