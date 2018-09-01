c60ba15a82b829a9fe787a24520cde21758695ea
TimVideos.us Robot <robot@timvideos.us>
2018-09-01 13:26:59 -0700

    Travis build #709 of or1k-rom-size-fix/v0.0.4-274-g41bfac0 for PLATFORM=saturn TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/41bfac0acb637cb37bae8deab651006cf0031af0

---
+++ archive/or1k-rom-size-fix/v0.0.4-274-g41bfac0/saturn/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_SATURN
+#error "Version mismatch - PLATFORM_SATURN not defined!"
+#endif
+const char* board = "saturn";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "41bfac0acb637cb37bae8deab651006cf0031af0";
+const char* git_branch = "or1k-rom-size-fix";
+const char* git_describe = "v0.0.4-274-g41bfac0";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
