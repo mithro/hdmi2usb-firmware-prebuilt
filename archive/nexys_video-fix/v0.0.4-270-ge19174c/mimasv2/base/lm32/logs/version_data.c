04f93f1de961ffa45315e3ffe300afb628e66c32
TimVideos.us Robot <robot@timvideos.us>
2018-09-01 12:08:53 -0700

    Travis build #707 of nexys_video-fix/v0.0.4-270-ge19174c for PLATFORM=mimasv2 TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/e19174cb79e9a6a4ef80a854d62879fe07ef858e

---
+++ archive/nexys_video-fix/v0.0.4-270-ge19174c/mimasv2/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MIMASV2
+#error "Version mismatch - PLATFORM_MIMASV2 not defined!"
+#endif
+const char* board = "mimasv2";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "e19174cb79e9a6a4ef80a854d62879fe07ef858e";
+const char* git_branch = "nexys_video-fix";
+const char* git_describe = "v0.0.4-270-ge19174c";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
