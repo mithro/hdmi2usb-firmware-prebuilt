2e85e6f5b6c34355b06ebf91ba512b67dbdbd77a
TimVideos.us Robot <robot@timvideos.us>
2018-09-01 14:18:46 -0700

    Travis build #711 of or1k-rom-size-fix/v0.0.4-274-g2d83f1b for PLATFORM=opsis TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/2d83f1b49534208d7d3a74b6f1fdc43339b8467c

---
+++ archive/or1k-rom-size-fix/v0.0.4-274-g2d83f1b/opsis/net/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "2d83f1b49534208d7d3a74b6f1fdc43339b8467c";
+const char* git_branch = "or1k-rom-size-fix";
+const char* git_describe = "v0.0.4-274-g2d83f1b";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
