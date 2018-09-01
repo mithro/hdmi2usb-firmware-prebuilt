9bf0c1631d299ebc6559f1655b813b0816746e82
TimVideos.us Robot <robot@timvideos.us>
2018-09-01 15:01:09 -0700

    Travis build #711 of or1k-rom-size-fix/v0.0.4-274-g2d83f1b for PLATFORM=arty TARGET=net CPU=or1k.linux FIRMWARE=linux
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/2d83f1b49534208d7d3a74b6f1fdc43339b8467c

---
+++ archive/or1k-rom-size-fix/v0.0.4-274-g2d83f1b/arty/net/or1k.linux/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ARTY
+#error "Version mismatch - PLATFORM_ARTY not defined!"
+#endif
+const char* board = "arty";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "2d83f1b49534208d7d3a74b6f1fdc43339b8467c";
+const char* git_branch = "or1k-rom-size-fix";
+const char* git_describe = "v0.0.4-274-g2d83f1b";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
