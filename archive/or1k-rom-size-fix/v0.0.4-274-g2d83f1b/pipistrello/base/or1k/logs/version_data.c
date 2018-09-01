54e7ff8e963720e16810676dd369259b7941edc2
TimVideos.us Robot <robot@timvideos.us>
2018-09-01 14:41:00 -0700

    Travis build #711 of or1k-rom-size-fix/v0.0.4-274-g2d83f1b for PLATFORM=pipistrello TARGET=base CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/2d83f1b49534208d7d3a74b6f1fdc43339b8467c

---
+++ archive/or1k-rom-size-fix/v0.0.4-274-g2d83f1b/pipistrello/base/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_PIPISTRELLO
+#error "Version mismatch - PLATFORM_PIPISTRELLO not defined!"
+#endif
+const char* board = "pipistrello";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "2d83f1b49534208d7d3a74b6f1fdc43339b8467c";
+const char* git_branch = "or1k-rom-size-fix";
+const char* git_describe = "v0.0.4-274-g2d83f1b";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
