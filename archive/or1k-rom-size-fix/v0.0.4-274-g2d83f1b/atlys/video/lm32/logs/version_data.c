693df2136c31f006b7b71241ebeda5e51561277e
TimVideos.us Robot <robot@timvideos.us>
2018-09-01 15:05:56 -0700

    Travis build #711 of or1k-rom-size-fix/v0.0.4-274-g2d83f1b for PLATFORM=atlys TARGET=video CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/2d83f1b49534208d7d3a74b6f1fdc43339b8467c

---
+++ archive/or1k-rom-size-fix/v0.0.4-274-g2d83f1b/atlys/video/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "2d83f1b49534208d7d3a74b6f1fdc43339b8467c";
+const char* git_branch = "or1k-rom-size-fix";
+const char* git_describe = "v0.0.4-274-g2d83f1b";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
