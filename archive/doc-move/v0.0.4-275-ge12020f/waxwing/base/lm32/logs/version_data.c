0efc99099c4adda5d2821cddb12714ca719a8957
TimVideos.us Robot <robot@timvideos.us>
2018-09-01 18:09:24 -0700

    Travis build #713 of doc-move/v0.0.4-275-ge12020f for PLATFORM=waxwing TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/e12020fafd68f706701607e629dd0f40f7acc427

---
+++ archive/doc-move/v0.0.4-275-ge12020f/waxwing/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_WAXWING
+#error "Version mismatch - PLATFORM_WAXWING not defined!"
+#endif
+const char* board = "waxwing";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "e12020fafd68f706701607e629dd0f40f7acc427";
+const char* git_branch = "doc-move";
+const char* git_describe = "v0.0.4-275-ge12020f";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
