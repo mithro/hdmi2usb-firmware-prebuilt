bad2c93c500f0378e9dddf31305ca0258c8f5645
TimVideos.us Robot <robot@timvideos.us>
2018-10-04 23:29:54 -0700

    Travis build #732 of autolaber-update/v0.0.4-357-g3fcf42d for PLATFORM=nexys_video TARGET=net CPU=or1k.linux FIRMWARE=linux
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/3fcf42d7c07aa78d3361aea80f9ecfea9a505f5a

---
+++ archive/autolaber-update/v0.0.4-357-g3fcf42d/nexys_video/net/or1k.linux/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NEXYS_VIDEO
+#error "Version mismatch - PLATFORM_NEXYS_VIDEO not defined!"
+#endif
+const char* board = "nexys_video";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "3fcf42d7c07aa78d3361aea80f9ecfea9a505f5a";
+const char* git_branch = "autolaber-update";
+const char* git_describe = "v0.0.4-357-g3fcf42d";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
