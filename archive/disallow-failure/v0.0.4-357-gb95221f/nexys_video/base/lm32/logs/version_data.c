4b2191cb0cccfe6bc2a1765c6bcdbde733ef4cf3
TimVideos.us Robot <robot@timvideos.us>
2018-10-04 19:29:53 -0700

    Travis build #731 of disallow-failure/v0.0.4-357-gb95221f for PLATFORM=nexys_video TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/b95221f37fffb2035fec05643a1791f1fc387010

---
+++ archive/disallow-failure/v0.0.4-357-gb95221f/nexys_video/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NEXYS_VIDEO
+#error "Version mismatch - PLATFORM_NEXYS_VIDEO not defined!"
+#endif
+const char* board = "nexys_video";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "b95221f37fffb2035fec05643a1791f1fc387010";
+const char* git_branch = "disallow-failure";
+const char* git_describe = "v0.0.4-357-gb95221f";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
