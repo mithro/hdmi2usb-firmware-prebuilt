d13f869aa29223d9e881baa8b9b0dd27dff0609d
TimVideos.us Robot <robot@timvideos.us>
2018-10-04 21:15:11 -0700

    Travis build #731 of disallow-failure/v0.0.4-357-gb95221f for PLATFORM=opsis TARGET=video CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/b95221f37fffb2035fec05643a1791f1fc387010

---
+++ archive/disallow-failure/v0.0.4-357-gb95221f/opsis/video/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "b95221f37fffb2035fec05643a1791f1fc387010";
+const char* git_branch = "disallow-failure";
+const char* git_describe = "v0.0.4-357-gb95221f";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
