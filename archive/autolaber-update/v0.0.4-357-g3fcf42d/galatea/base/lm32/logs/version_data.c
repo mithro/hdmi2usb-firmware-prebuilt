f20dac872ad31008a86b705f44c6b90c88952ee6
TimVideos.us Robot <robot@timvideos.us>
2018-10-04 21:30:32 -0700

    Travis build #732 of autolaber-update/v0.0.4-357-g3fcf42d for PLATFORM=galatea TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/3fcf42d7c07aa78d3361aea80f9ecfea9a505f5a

---
+++ archive/autolaber-update/v0.0.4-357-g3fcf42d/galatea/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_GALATEA
+#error "Version mismatch - PLATFORM_GALATEA not defined!"
+#endif
+const char* board = "galatea";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "3fcf42d7c07aa78d3361aea80f9ecfea9a505f5a";
+const char* git_branch = "autolaber-update";
+const char* git_describe = "v0.0.4-357-g3fcf42d";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
