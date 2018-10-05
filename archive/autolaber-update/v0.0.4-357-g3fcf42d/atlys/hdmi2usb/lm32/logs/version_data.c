d1a024476bf2e06867f4a0c632d22a89c62fe4e2
TimVideos.us Robot <robot@timvideos.us>
2018-10-04 23:52:09 -0700

    Travis build #732 of autolaber-update/v0.0.4-357-g3fcf42d for PLATFORM=atlys TARGET=hdmi2usb CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/3fcf42d7c07aa78d3361aea80f9ecfea9a505f5a

---
+++ archive/autolaber-update/v0.0.4-357-g3fcf42d/atlys/hdmi2usb/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_HDMI2USB
+#error "Version mismatch - TARGET_HDMI2USB not defined!"
+#endif
+const char* target = "hdmi2usb";
+
+const char* git_commit = "3fcf42d7c07aa78d3361aea80f9ecfea9a505f5a";
+const char* git_branch = "autolaber-update";
+const char* git_describe = "v0.0.4-357-g3fcf42d";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
