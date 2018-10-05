9c1a475d7e661e90e5fe30656c6519284df12401
TimVideos.us Robot <robot@timvideos.us>
2018-10-05 00:03:27 -0700

    Travis build #732 of autolaber-update/v0.0.4-357-g3fcf42d for PLATFORM=opsis TARGET=hdmi2usb CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/3fcf42d7c07aa78d3361aea80f9ecfea9a505f5a

---
+++ archive/autolaber-update/v0.0.4-357-g3fcf42d/opsis/hdmi2usb/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_HDMI2USB
+#error "Version mismatch - TARGET_HDMI2USB not defined!"
+#endif
+const char* target = "hdmi2usb";
+
+const char* git_commit = "3fcf42d7c07aa78d3361aea80f9ecfea9a505f5a";
+const char* git_branch = "autolaber-update";
+const char* git_describe = "v0.0.4-357-g3fcf42d";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
