efd172ef4909e9b67f56bafebbe8d904e4f8eb0a
TimVideos.us Robot <robot@timvideos.us>
2019-02-12 22:46:00 -0800

    Travis build #743 of update-submodules/v0.0.4-454-g97aad34 for PLATFORM=opsis TARGET=net CPU=lm32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/97aad34e440a23b8cd87770002dbf545afd880d7

---
+++ archive/update-submodules/v0.0.4-454-g97aad34/opsis/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_OPSIS
+#error "Version mismatch - PLATFORM_OPSIS not defined!"
+#endif
+const char* board = "opsis";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "97aad34e440a23b8cd87770002dbf545afd880d7";
+const char* git_branch = "update-submodules";
+const char* git_describe = "v0.0.4-454-g97aad34";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
