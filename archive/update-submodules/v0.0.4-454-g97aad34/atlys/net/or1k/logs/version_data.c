4fefbfe51f2bc2baa24bfc888f69e9c9c7532edb
TimVideos.us Robot <robot@timvideos.us>
2019-02-12 23:31:37 -0800

    Travis build #743 of update-submodules/v0.0.4-454-g97aad34 for PLATFORM=atlys TARGET=net CPU=or1k FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/97aad34e440a23b8cd87770002dbf545afd880d7

---
+++ archive/update-submodules/v0.0.4-454-g97aad34/atlys/net/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "97aad34e440a23b8cd87770002dbf545afd880d7";
+const char* git_branch = "update-submodules";
+const char* git_describe = "v0.0.4-454-g97aad34";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
