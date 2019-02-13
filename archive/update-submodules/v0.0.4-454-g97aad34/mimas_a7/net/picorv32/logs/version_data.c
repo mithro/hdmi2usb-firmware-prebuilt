4bf4a4ea59ed7a811d937cadf9bfd9e2c3ad187f
TimVideos.us Robot <robot@timvideos.us>
2019-02-13 00:11:38 -0800

    Travis build #743 of update-submodules/v0.0.4-454-g97aad34 for PLATFORM=mimas_a7 TARGET=net CPU=picorv32 FIRMWARE=firmware
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/97aad34e440a23b8cd87770002dbf545afd880d7

---
+++ archive/update-submodules/v0.0.4-454-g97aad34/mimas_a7/net/picorv32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MIMAS_A7
+#error "Version mismatch - PLATFORM_MIMAS_A7 not defined!"
+#endif
+const char* board = "mimas_a7";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "97aad34e440a23b8cd87770002dbf545afd880d7";
+const char* git_branch = "update-submodules";
+const char* git_describe = "v0.0.4-454-g97aad34";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
