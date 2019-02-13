3fa64aef4b491c1ef58855fc5f26298b00be84ed
TimVideos.us Robot <robot@timvideos.us>
2019-02-13 00:49:06 -0800

    Travis build #743 of update-submodules/v0.0.4-454-g97aad34 for PLATFORM=nexys_video TARGET=net CPU=or1k.linux FIRMWARE=linux
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/97aad34e440a23b8cd87770002dbf545afd880d7

---
+++ archive/update-submodules/v0.0.4-454-g97aad34/nexys_video/net/or1k.linux/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_NEXYS_VIDEO
+#error "Version mismatch - PLATFORM_NEXYS_VIDEO not defined!"
+#endif
+const char* board = "nexys_video";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "97aad34e440a23b8cd87770002dbf545afd880d7";
+const char* git_branch = "update-submodules";
+const char* git_describe = "v0.0.4-454-g97aad34";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
