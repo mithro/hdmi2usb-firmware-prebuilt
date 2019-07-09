d8ac221d1118025495c5c9ac7618f426ee23390a
TimVideos.us Robot <robot@timvideos.us>
2019-07-08 23:57:59 -0500

    Travis build #748 of master/v0.0.4-539-g7ec7417 for PLATFORM=atlys TARGET=net CPU=mor1kx.linux FIRMWARE=linux
    From https://github.com/mithro/HDMI2USB-litex-firmware/tree/7ec741741e4b2564bcb2b3e3b3fc4161bed8c9a8

---
+++ archive/master/v0.0.4-539-g7ec7417/atlys/net/mor1kx.linux/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "7ec741741e4b2564bcb2b3e3b3fc4161bed8c9a8";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-539-g7ec7417";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
